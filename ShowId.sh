603145de743c90f110d4cd49fbd5fc53478f21e0
DokBak <photoismylife0216@gmail.com>
2023-01-04 00:40:11 +0900

    id 명령어 설명 쉘:예시 순번 수정

--- ShowId.sh
+++ ShowId.sh
@@ -18,15 +18,16 @@ function ShowId(){
     id -F
     echo
     echo " 옵션 -u : 유효한 사용자 아이디를 숫자로 표기한다."
-    echo " 예시2) id -u"
+    echo " 예시3) id -u"
     id -u
     echo
     echo " 옵션 -un : whoami와 같은 출력"
-    echo " 예시2) id -un"
+    echo " 예시4) id -un"
     id -un
     echo
     echo "## 사용자 정보 출력 종료 ##"
     echo 
 }
+
 
 ShowId
