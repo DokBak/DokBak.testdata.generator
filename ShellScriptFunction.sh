ec8932c7f123fd1b7e47484035dc44636222c469
DokBak <photoismylife0216@gmail.com>
2023-01-15 23:31:41 +0900

    쉘 스크립트 함수 쉘 : 함수 선언 방법 추가

--- ShellScriptFunction.sh
+++ ShellScriptFunction.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 #-------------------------
 # 테스트 쉘 : 쉘 스크립트 함수
 #-------------------------
@@ -14,7 +15,13 @@ function ShellScriptFunction(){
     echo " 부가설명) 함수1 함수2 파라미터를 작성하여도 함수1의 파라미터가 2개인것으로 인식되어 실행된다."
     TestFunc1 TestFunc2 TestParam2
     echo 
-    echo "## 쉘 스크립트 실행 종료 ##"
+    echo " 부가설명) 함수 선언시 function을 생략하여 작성하는 방법으로 함수를 선언하여도 된다."
+    TestFunc3
+    echo 
+    echo " 부가설명) 함수명뒤의 () 소괄호를 작성하지 않아도 함수로 인식하여 실행된다."
+    TestFunc4
+    echo
+    echo "## 쉘 스크립트 함수 종료 ##"
     echo 
 }
 
@@ -29,5 +36,9 @@ function TestFunc2(){
     echo "TestFunc2, Param1 : $1"
     echo "TestFunc2 함수 종료"
 }
+
+TestFunc3() { echo "TestFunc3 print1"; echo "TestFunc3 print2"; }
+
+function TestFunc4 { echo "TestFunc4 print1"; echo "TestFunc4 print2"; }
 
 ShellScriptFunction
