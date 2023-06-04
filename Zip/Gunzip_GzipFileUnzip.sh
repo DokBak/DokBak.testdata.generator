9d8c4b7b79596099de02da4921f088a4640c66bf
DokBak <photoismylife0216@gmail.com>
2023-06-05 01:01:12 +0900

    FIX : Gunzip_GzipFileUnzip.sh
    gunzip Command
    Not Option

--- Zip/Gunzip_GzipFileUnzip.sh
+++ Zip/Gunzip_GzipFileUnzip.sh
@@ -3,14 +3,15 @@
 #------------------------------------------------------------
 # Cmd Test Shell : [Gunzip]_[GzipFileUnZip]
 #------------------------------------------------------------
-function GzipUnzip(){
+function Gunzip_GzipFileUnzip(){
     echo 
-    echo "## gzip Start ##"
+    echo " ## gunzip Start ## "
     echo
     # Basic Information Start
     echo
-    echo " Command  : gzip"
-    echo " HowToUse : gzip [Option] [Argument1]"
+    echo " Command  : gunzip "
+    echo " HowToUse : gunzip [Option] [Argument1] "
+    echo "            [Argument1] : gzipFile_Path "
     echo
     # Basic Information End
     # Preparation Start
@@ -19,25 +20,25 @@ function GzipUnzip(){
     echo " mkdir -p `pwd`/Gunzip_CMD_TestFolder "
     mkdir -p `pwd`/Gunzip_CMD_TestFolder
     # Preparation : Create File
-    echo " echo 'TestFile1' > `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt"
-    echo "TestFile1" > Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt
-    echo " echo 'TestFile2' > `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt"
-    echo "TestFile2" > Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt
+    echo " echo 'TestFile1' > `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt "
+    echo " TestFile1 " > Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt
+    echo " echo 'TestFile2' > `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt "
+    echo " TestFile2 " > Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt
     # Preparation : Create gzip File
-    echo " gzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt; gzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt"
+    echo " gzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt; gzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt "
     gzip Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt; gzip Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile2.txt
     echo
     # Preparation End
     # Example Start
     # Example 01 (Not Option)
     echo
-    echo " Gunzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt.gz"
+    echo " gunzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt.gz "
     gunzip `pwd`/Gunzip_CMD_TestFolder/Gunzip_CMD_TestFile1.txt.gz
     echo
     # Example End
     echo
-    echo "## gzip End ##"
+    echo " ## gunzip End ## "
     echo 
 }
 
-GzipUnzip
+Gunzip_GzipFileUnzip
