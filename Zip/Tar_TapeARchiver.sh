a60798d557825ff0cb45f2ae3f07dafc7a62350e
DokBak <photoismylife0216@gmail.com>
2023-06-07 02:35:25 +0900

    FIX : Tar_TapeARchiver.sh
    Opntion cvf
    Option tvf
    Option xvf
    Option rvf
    Option uvf
    Option xvf -C

--- Zip/Tar_TapeARchiver.sh
+++ Zip/Tar_TapeARchiver.sh
@@ -1,89 +1,102 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 여래 개의 파일을 하나의 파일로 묶거나 푸는 처리
-#-------------------------
-function TapeARchiver(){
+#------------------------------------------------------------
+# Cmd Test Shell : [tar]_[TapeARchiver]
+#------------------------------------------------------------
+function tar_TapeARchiver(){
     echo 
-    echo "## 여래 개의 파일을 하나의 파일로 묶거나 푸는 처리 시작 ##"
-    echo " 명령어  : tar"
-    echo " 사용방법 : tar [옵션] [파일명] [대상]"
-    echo " 기본설명 : 데이터의 크기를 줄이기 위한 파일 압축 작업이 없이 파일을 하나로 묶는데 사용된다."
+    echo "## tar Start ##"
     echo
-    echo " 사전실행) tar 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir Tar_CMD_TestFolder"
-    mkdir -p Tar_CMD_TestFolder/Tar_CMD_TestFolder2
-    echo "    echo 'TestFile1' > Tar_CMD_TestFolder/Tar_CMD_TestFile1.txt"
-    echo "TestFile1" > Tar_CMD_TestFolder/Tar_CMD_TestFile1.txt
-    echo "    echo 'TestFile2' > Tar_CMD_TestFolder/Tar_CMD_TestFile2.txt"
-    echo "TestFile2" > Tar_CMD_TestFolder/Tar_CMD_TestFile2.txt
-    echo "    echo 'TestFile3' > Tar_CMD_TestFolder/Tar_CMD_TestFile3.txt"
-    echo "TestFile3" > Tar_CMD_TestFolder/Tar_CMD_TestFile3.txt
-    echo "    cd Tar_CMD_TestFolder"
-    cd Tar_CMD_TestFolder
+    # Basic Information Start
     echo
-    echo " 기능 -c : tar 아카이브 생성, 기존 아카이브 덮어 쓰기 -> create"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " 현재 디렉토리의 모든 파일과 디렉토리를 TestTar1.tar로 묶기"
-    echo " 예시1) tar cvf TestTar1.tar * "
-    tar cvf TestTar1.tar *
+    echo " Command  : tar "
+    echo " HowToUse : tar [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : tarFile_Path "
+    echo "            [Argument2] : targetFile_Path "
+    echo "            [Option : -v] : tar Command Preview "
+    echo "            [Option : -f] : tar Archive File (default) "
+    echo "            [Option : -c] : tar Archive Create (overwrite) "
+    echo "            [Option : -t] : tar Archive File List Show "
+    echo "            [Option : -x] : tar Archive Extracting To Current Folder "
+    echo "            [Option : -r] : tar Archive File Add (append) "
+    echo "            [Option : -u] : tar Archive File Add (update) "
+    echo "            [Option : -C] : tar Archive Extracting To Specify Folder "    
     echo
-    echo " 기능 -c : tar 아카이브 생성, 기존 아카이브 덮어 쓰기 -> create"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " 지정한 파일을 TestTar2.tar로 묶기"
-    echo " 예시2) tar cvf TestTar2.tar Tar_CMD_TestFile1.txt Tar_CMD_TestFile2.txt "
-    tar cvf TestTar2.tar Tar_CMD_TestFile1.txt Tar_CMD_TestFile2.txt
+    # Basic Information End
+    # Preparation Start
     echo
-    echo " 기능 -t : tar 아카이브에 포함된 내용 확인 -> list"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " TestTar1.tar를 현재 디렉토리에 풀기"
-    echo " 예시3) tar tvf TestTar1.tar"
-    tar tvf TestTar1.tar
-    echo "       tar tvf TestTar2.tar"
-    tar tvf TestTar2.tar
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/tar_CMD_TestFolder/tar_CMD_TestFolder2 "
+    mkdir -p `pwd`/tar_CMD_TestFolder/tar_CMD_TestFolder2
+    # Preparation : Create File
+    echo " echo 'TestFile1' > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt "
+    echo " TestFile1" > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt
+    echo " echo 'TestFile2' > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt "
+    echo " TestFile2" > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt
+    echo " echo 'TestFile3' > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFolder2/tar_CMD_TestFile3.txt "
+    echo " TestFile3" > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFolder2/tar_CMD_TestFile3.txt
+    echo " Tcd `pwd`/tar_CMD_TestFolder/ "
     echo
-    echo " 기능 -x : tar 아카이브 현재 디렉토리에 풀기 -> extract"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " TestTar1.tar를 현재 디렉토리에 풀기"
-    echo " 예시4) tar xvf TestTar1.tar"
-    tar xvf TestTar1.tar
+    # Preparation End
+    # Example Start
+    # Example 01 (Option cvf : Creating a tar Archive1)
     echo
-    echo " 기능 -x : tar 아카이브 현재 디렉토리에 풀기 -> extract"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " 옵션 -C : -C 이후 작성된 디렉토리에서 작업이 수행"    
-    echo " TestTar2.tar를 지정된 디렉토리에 풀기"
-    echo " 예시5) tar xvf TestTar2.tar -C Tar_CMD_TestFolder2"
-    tar xvf TestTar2.tar -C Tar_CMD_TestFolder2
+    echo " tar cvf `pwd`/tar_CMD_TestFolder/Testtar1.tar `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt "
+    tar cvf `pwd`/tar_CMD_TestFolder/Testtar1.tar `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt
+    echo    
+    # Example 02 (Option cvf : Creating a tar Archive2)
     echo
-    echo " 기능 -r : tar 아카이브에 파일 추가 -> append"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " 지정한 파일을 TestTar3.tar에 추가하기"
-    echo " 예시2) tar rvf TestTar3.tar Tar_CMD_TestFile3.txt"
-    cp TestTar2.tar TestTar3.tar
-    tar tvf TestTar3.tar
-    tar rvf TestTar3.tar Tar_CMD_TestFile3.txt
-    echo "       tar tvf TestTar3.tar"
-    tar tvf TestTar3.tar 
+    echo " tar cvf `pwd`/tar_CMD_TestFolder/Testtar2.tar `pwd`/tar_CMD_TestFolder/* "
+    tar cvf `pwd`/tar_CMD_TestFolder/Testtar2.tar `pwd`/tar_CMD_TestFolder/*
+    echo    
+    # Example 03 (Option tvf : tar Archive Show)
     echo
-    echo " 기능 -u : tar 아카이브에 파일 추가 (아카이브의 해당 항목보다 새로운 수정날짜가 있는 경우 업데이트) -> update"
-    echo " 기능 -v : 처리되는 파일을 자세히 나열"
-    echo " 기능 -f : 대상 tar 아카이브 지정(기본 옵션)"
-    echo " 지정한 파일을 TestTar3.tar에 추가하기"
-    echo " 예시2) tar uvf TestTar4.tar Tar_CMD_TestFile3.txt"
-    cp TestTar2.tar TestTar4.tar
-    tar tvf TestTar4.tar
-    tar uvf TestTar4.tar Tar_CMD_TestFile3.txt
-    echo "       tar tvf TestTar4.tar"
-    tar tvf TestTar4.tar 
+    echo " tar tvf `pwd`/tar_CMD_TestFolder/Testtar1.tar "
+    tar tvf `pwd`/tar_CMD_TestFolder/Testtar1.tar
+    echo " tar tvf `pwd`/tar_CMD_TestFolder/Testtar2.tar "
+    tar tvf `pwd`/tar_CMD_TestFolder/Testtar2.tar
     echo
-    echo "## 여래 개의 파일을 하나의 파일로 묶거나 푸는 처리 종료 ##"
+    # Example 04 (Option xvf : Extracting a tar Archive)
+    echo
+    echo " tar xvf `pwd`/tar_CMD_TestFolder/Testtar1.tar "
+    tar xvf `pwd`/tar_CMD_TestFolder/Testtar1.tar
+    echo
+    # Example 05 (Option xvf : Extracting a tar Archive)
+    #            (Option -C : To specify a directory)
+    echo
+    echo " tar xvf `pwd`/tar_CMD_TestFolder/Testtar2.tar -C `pwd`/tar_CMD_TestFolder/tar_CMD_TestFolder2 "
+    tar xvf `pwd`/tar_CMD_TestFolder/Testtar2.tar -C `pwd`/tar_CMD_TestFolder/tar_CMD_TestFolder2
+    echo
+    # Example 06 (Option rvf : Add File to tar Archive)
+    echo
+    echo " cp `pwd`/tar_CMD_TestFolder/Testtar1.tar `pwd`/tar_CMD_TestFolder/Testtar3.tar "
+    cp `pwd`/tar_CMD_TestFolder/Testtar1.tar `pwd`/tar_CMD_TestFolder/Testtar3.tar
+    echo " tar tvf `pwd`/tar_CMD_TestFolder/Testtar3.tar "
+    tar tvf `pwd`/tar_CMD_TestFolder/Testtar3.tar
+    echo " echo 'r TestFile1' > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt "
+    echo " r TestFile1" > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt
+    echo " tar rvf `pwd`/tar_CMD_TestFolder/Testtar3.tar `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt "
+    tar rvf `pwd`/tar_CMD_TestFolder/Testtar3.tar `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile1.txt
+    echo " tar tvf `pwd`/tar_CMD_TestFolder/Testtar3.tar "
+    tar tvf `pwd`/tar_CMD_TestFolder/Testtar3.tar 
+    echo
+    # Example 07 (Option uvf : Update File to tar Archive)
+    echo
+    echo " cp `pwd`/tar_CMD_TestFolder/Testtar1.tar `pwd`/tar_CMD_TestFolder/Testtar4.tar "
+    cp `pwd`/tar_CMD_TestFolder/Testtar1.tar `pwd`/tar_CMD_TestFolder/Testtar4.tar
+    echo " tar tvf `pwd`/tar_CMD_TestFolder/Testtar4.tar "
+    tar tvf `pwd`/tar_CMD_TestFolder/Testtar4.tar
+    echo " echo 'u TestFile2' > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt "
+    echo " u TestFile2" > `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt
+    echo " tar uvf `pwd`/tar_CMD_TestFolder/Testtar4.tar `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt "
+    tar uvf `pwd`/tar_CMD_TestFolder/Testtar4.tar `pwd`/tar_CMD_TestFolder/tar_CMD_TestFile2.txt
+    echo " tar tvf `pwd`/tar_CMD_TestFolder/Testtar4.tar"
+    tar tvf `pwd`/tar_CMD_TestFolder/Testtar4.tar 
+    echo
+    # Example End
+    echo
+    echo "## tar End ##"
     echo 
 }
 
-TapeARchiver 
+tar_TapeARchiver 
