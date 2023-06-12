7a4777477ef91073381634f0127fb4924442669d
DokBak <photoismylife0216@gmail.com>
2023-06-12 23:20:09 +0900

    FIX : Unset_DeleteVariable.sh
    Not Option

--- System/Unset_DeleteVariable.sh
+++ System/Unset_DeleteVariable.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 환경 변수 삭제
-#-------------------------
-function DeleteVariable(){
+#------------------------------------------------------------
+# Cmd Test Shell : [unset]_[DeleteVariable]
+#------------------------------------------------------------
+function unset_DeleteVariable(){
     echo 
-    echo "## 환경 변수 삭제 시작 ##"
-    echo " 명령어  : unset"
-    echo " 사용방법 : unset [변수명]"
-    echo " 기본설명 : unset 이후 작성된 변수(전역변수)를 삭제한다. 영구적으로 삭제 되는 것이 아닌 프로세스가 종료되기 전까지 삭제. 영구적으로 삭제하려면 환경설정파일을 수정하여 삭제하여야한다."
+    echo "## unset Start ##"
     echo
-    echo " 사전실행) export GLOBALVAR=GLOBALDATA"
+    # Basic Information Start
+    echo
+    echo " Command  : unset "
+    echo " HowToUse : unset "  
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : export
+    echo " export GLOBALVAR=GLOBALDATA "
     export GLOBALVAR=GLOBALDATA
-    echo "         env | grep GLOBALVAR"
+    echo " env | grep GLOBALVAR "
     env | grep GLOBALVAR
     echo
-    echo " 예시1) unset GLOBALVAR"
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " unset GLOBALVAR"
     unset GLOBALVAR
-    echo "       env | grep GLOBALVAR"
+    echo " env | grep GLOBALVAR"
     env | grep GLOBALVAR
     echo
-    echo "## 환경 변수 삭제 종료 ##"
+    # Example End
+    echo
+    echo "## unset End ##"
     echo 
 }
 
-DeleteVariable
+unset_DeleteVariable 
