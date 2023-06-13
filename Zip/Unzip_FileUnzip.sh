d804cb161c5069c4c93473a406ff62dbdeb01f49
DokBak <photoismylife0216@gmail.com>
2023-06-14 02:41:25 +0900

    FIX : Unzip_FileUnzip.sh
    Not Option
    Option -l
    Option -q
    Option -d

--- Zip/Unzip_FileUnzip.sh
+++ Zip/Unzip_FileUnzip.sh
@@ -10,8 +10,12 @@ function unzip_FileUnzip(){
     # Basic Information Start
     echo
     echo " Command  : unzip"
-    echo " HowToUse : unzip [Option] [Argument1] "
+    echo " HowToUse : unzip [Option] [Argument1] [Argument2] "
     echo "            [Argument1] : unzipFile_Path "
+    echo "            [Argument2] : unzipFile_Directory(-d Option) "
+    echo "            [Option : -l] : zip File List Show) "
+    echo "            [Option : -q] : No Message) "
+    echo "            [Option : -d] : Unzip Directory Select) "
     echo
     # Basic Information End
     # Preparation Start
@@ -23,15 +27,30 @@ function unzip_FileUnzip(){
     echo " echo 'TestFile1' > `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.txt "
     echo " TestFile1 " > `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.txt
     # Preparation : Create unzip File
-    echo " zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.unzip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.txt "
-    zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.unzip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.txt 
+    echo " zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.txt "
+    zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.txt 
     echo
     # Preparation End
     # Example Start
     # Example 01 (Not Option : Current Folder Unzip)
     echo
-    echo " unzip unzip_CMD_TestFolder/unzip_CMD_TestFile1.unzip "
-    unzip unzip_CMD_TestFolder/unzip_CMD_TestFile1.unzip
+    echo " unzip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip "
+    unzip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip
+    echo
+    # Example 02 (Option -l : zip File List Show)
+    echo
+    echo " unzip -l `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip "
+    unzip -l `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip 
+    echo
+    # Example 03 (Option -q : No Message)
+    echo
+    echo " unzip -q `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip "
+    unzip -q `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip 
+    echo
+    # Example 04 (Option -d : Unzip Directory Select)
+    echo
+    echo " unzip -d `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFolder2/ "
+    unzip `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFile1.zip -d `pwd`/unzip_CMD_TestFolder/unzip_CMD_TestFolder2/
     echo
     # Example End
     echo
