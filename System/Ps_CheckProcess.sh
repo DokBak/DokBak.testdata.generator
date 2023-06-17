d15b5ffce45999d958b6865bdc0a20c85cce6ff4
DokBak <photoismylife0216@gmail.com>
2023-06-17 23:25:04 +0900

    FIX : Ps_CheckProcess.sh
    Not Option
    Option -f
    Option -e
    Option -l

--- System/Ps_CheckProcess.sh
+++ System/Ps_CheckProcess.sh
@@ -1,32 +1,50 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 실행중인 프로세스 출력
-#-------------------------
-function CheckProcess(){
+#------------------------------------------------------------
+# Cmd Test Shell : [ps]_[CheckProcess]
+#------------------------------------------------------------
+function ps_CheckProcess(){
     echo 
-    echo "## 실행중인 프로세스 출력 시작 ##"
-    echo " 명령어  : ps "
-    echo " 사용방법 : ps "
-    echo " 기본설명 : 현재 실행중인 프로세스를 표시해준다. PID : 프로세스번호, TTY : 프로세스가 연결된 터미널, Time : 총 CPU 사용시간, COMMAND : 프로세스의 실행 명령행"
+    echo "## ps Start ##"
     echo
-    echo " 예시1) ps"
+    # Basic Information Start
+    echo
+    echo " Command  : ps "
+    echo " HowToUse : ps [Option] "  
+    echo "            [Option : -f] : Process Info "
+    echo "            [Option : -e] : All User Process List "
+    echo "            [Option : -l] : Process Info + resource, runtime "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " ps "
     ps
     echo
-    echo " 옵션 -A : 다른 사용자에 대한 모든 프로세스까지 표시한다.(-e 도 동일하다.)"
-    echo " 예시2) ps -A"
-    ps -A
+    # Example 02 (Option -f : Process Info)
     echo
-    echo " 옵션 -a : 사용자가 소유하고 있는 모든 프로세스를 표시한다."
-    echo " 예시2) ps -a"
-    ps -a
-    echo
-    echo " 옵션 -f : 풀 포멧으로 정보를 출력해준다. UID, PID, PPID 등도 출력한다."
-    echo " 예시2) ps -f"
+    echo " ps -f "
     ps -f
     echo
-    echo "## 실행중인 프로세스 출력 종료 ##"
+    # Example 03 (Option -e : All User Process List)
+    echo
+    echo " ps -e "
+    ps -e
+    echo
+    # Example 04 (Option -l : Process Info + resource, runtime)
+    echo
+    echo " ps -l "
+    ps -l
+    echo
+    # Example End
+    echo
+    echo "## ps End ##"
     echo 
 }
 
-CheckProcess
+ps_CheckProcess
