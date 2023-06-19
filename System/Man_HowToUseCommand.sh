970a658cb975019e839c3b86df618ddb2e0b7b20
DokBak <photoismylife0216@gmail.com>
2023-06-19 15:51:20 +0900

    FIX : Man_HowToUseCommand.sh
    Not Option

--- System/Man_HowToUseCommand.sh
+++ System/Man_HowToUseCommand.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 리눅스 명령어 사용법 확인
-#-------------------------
-function HowToUseCommand(){
+#------------------------------------------------------------
+# Cmd Test Shell : [man]_[HowToUseCommand]
+#------------------------------------------------------------
+function man_NetworkConnecting(){
     echo 
-    echo "## 리눅스 명령어 사용법 확인 시작 ##"
-    echo " 명령어  : man"
-    echo " 사용방법 : man [명령어]"
-    echo " 기본설명 : [명령어]의 사용법, 혹은 옵션의 상세 내용을 출력해준다. 편집기로 열리는데 사용법 확인후 q 입력후 엔터로 종료한다."
+    echo "## man Start ##"
     echo
-    echo " 예시1) man cd"
-    man cd 
+    # Basic Information Start
     echo
-    echo "## 리눅스 명령어 사용법 확인 종료 ##"
+    echo " Command  : man "
+    echo " HowToUse : man [Option] [Argument1] " 
+    echo "            [Argument1] : Command " 
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " man cd"
+    man cd
+    echo
+    # Example End
+    echo
+    echo "## man End ##"
     echo 
 }
 
-HowToUseCommand
+man_NetworkConnecting
