45b3a01439d843e70847b6070cbad4e7058b7b24
DokBak <photoismylife0216@gmail.com>
2023-06-23 01:41:42 +0900

    FIX : Env_EnvironmentalVariable.sh
    Not Option
    Option -0
    Option -u

--- System/Env_EnvironmentalVariable.sh
+++ System/Env_EnvironmentalVariable.sh
@@ -1,37 +1,51 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 전역 변수 조회
-#-------------------------
-function EnvironmentalVariable(){
+#------------------------------------------------------------
+# Cmd Test Shell : [Env]_[EnvironmentalVariable]
+#------------------------------------------------------------
+function Env_EnvironmentalVariable(){
     echo 
-    echo "## 전역 변수 조회 시작 ##"
-    echo " 명령어  : env"
-    echo " 사용방법 : env"
-    echo " 기본설명 : 전역 변수 조회, export 명령어로 일시적으로 전역 변수를 선언 할 수 있다."
-    echo "          모든 사용자가 사용가능하도록 등록하고 싶을 경우에는 쉘 환경 설정 파일을 수정한다."
-    echo " zsh쉘 변경 시작 : vi ~/.zshrc, zsh쉘 수정 내용 반영 : source ~/.zshrc"
-    echo " bash쉘 변경 시작 : vi ~/bash_profile, bahs쉘 수정 내용 반영 : source ~/bash_profile"
+    echo "## Env Start ##"
     echo
-    echo " 예시1) env"
+    # Basic Information Start
+    echo
+    echo " Command  : Env"
+    echo " HowToUse : Env [Option] "
+    echo "            [Option : -0] : Not NewLine) "
+    echo "            [Option : -u] : Not include) "
+    # Registration Variable #
+    echo " zsh Variable Registration  : vi ~/.zshrc "
+    echo " zsh Variable Apply         : source ~/.zshrc "
+    echo " bash Variable Registration : vi ~/bash_profile "
+    echo " bash Variable Apply        : source ~/bash_profile "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo " export TESTenv='TESTenvV' "
+    export TESTenv='TESTenvV'
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " env "
     env
+    echo 
+    # Example 02 (Option -0 : Not NewLine )
     echo
-    echo " 옵션 -0 : 환경 변수들이 새줄로 표시되지 않고 NUL로 표시되어 모든 전역 변수들이 한 줄에 출력된다."
-    echo " 예시2) env -0"
+    echo " env -0 "
     env -0
     echo
-    echo " 옵션 -u : 해당 환경 변수를 출력하지 않고 실행시킨다."
-    echo " 예시3) env -u TESTenv"
-    export TESTenv="TESTenvV"
-    echo "새로 등록한 변수 확인 TESTenv : ${TESTenv}"
+    # Example 03 (Option -u : Not include )
+    echo
+    echo " env -u TESTenv "
     env -u TESTenv
     echo
-    echo " 옵션 -i : 설정된 환경 변수를 출력하지 않고 지정한 임의의 값으로 출력시킨다.(환경변수 자체가 수정되는 것은 아니다.)"
-    echo " 예시4) env -i TESTenv=TESTenvVV"
-    env -i TESTenv=TESTenvVV
+    # Example End
     echo
-    echo "## 전역 변수 조회 종료 ##"
+    echo "## Env End ##"
     echo 
 }
 
-EnvironmentalVariable
+Env_EnvironmentalVariable
