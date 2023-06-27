cd15fb5a67f4be49b8e17a42e30a69aa563f770c
DokBak <photoismylife0216@gmail.com>
2023-06-28 01:26:16 +0900

    FIX : Date_CurrentDate.sh
    Not Option
    Option -u
    Option -R

--- Date/Date_CurrentDate.sh
+++ Date/Date_CurrentDate.sh
@@ -1,135 +1,356 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 현재 시간 출력 처리
-#-------------------------
-function CurrentDate(){
+#------------------------------------------------------------
+# Cmd Test Shell : [Date]_[CurrentDate]
+#------------------------------------------------------------
+function Date_CurrentDate(){
     echo 
-    echo "## 현재 시간 출력 처리 시작 ##"
-    echo " 명령어 : date"
-    echo " 사용방법 : date [파라미터없음]"
-    echo " 기본설명 : 현재 시간을 출력하는 명령어 (locale : ko_KR.UTF-8 기준 )"
-    echo " 예시1) date 로케일에 따른 형식 출력"
+    echo "## Date Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : Date"
+    echo " HowToUse : Date [Option] [Argument1] [Argument2] ..."
+    echo "            [Argument1] : tarFile_Path "
+    echo "            [Option : -q] : No Message) "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : Format Output According to Date Locale )
+    echo
+    echo " locale Change : KR "
+    echo ' export LC_ALL="ko_KR.UTF-8" '
+    export LC_ALL="ko_KR.UTF-8"
+    echo
+    echo " locale " 
+    locale 
+    echo
+    echo " date "
     date
     echo
-    echo " 예시2) date 형식 지정 방식"
-    date "+%Y년 %m월 %d일 %A %H시 %M분 %S초 %Z"
+    echo " Korea Locale Output Format "
+    echo ' date "+%Y년 %h월 %d일 %A %H시 %M분 %S초 %Z" '
+    date "+%Y년%h월 %d일 %A %H시 %M분 %S초 %Z"
     echo
-    echo " - 날짜 + 시간 출력"
-    echo " 출력형식 종류(%c) : %a %b/%e %H:%M:%S %Y (locale : ko_KR.UTF-8)"
+    echo " locale Change : JP "
+    echo ' export LC_ALL="ja_JP.UTF-8" '
+    export LC_ALL="ja_JP.UTF-8"
+    echo
+    echo " locale " 
+    locale 
+    echo
+    echo ' date ' 
+    date
+    echo
+    echo " JP Locale Output Format "
+    echo ' date "+%Y年%h月%d日 %a曜日 %H時%M分%S秒 %Z" '
+    date "+%Y年%h月%d日 %a曜日 %H時%M分%S秒 %Z"
+    echo
+    echo " locale Change : US "
+    echo ' export LC_ALL="en_US.UTF-8" '
+    export LC_ALL="en_US.UTF-8"
+    echo
+    echo " locale " 
+    locale 
+    echo
+    echo ' date ' 
+    date
+    echo
+    echo " US Locale Output Format "
+    echo ' date "+%a %b %d %H:%M:%S %Z %Y" '
+    date "+%a %b %d %H:%M:%S %Z %Y"
+    echo
+    # Example 02 (Not Option : %c [week months days time(hours:minutes:seconds) years] )
+    echo 
+    echo " %c Type Print "
+    echo " [week months days time(hours:minutes:seconds) years] "
+    echo " [ %c : (KR) 수  6/21 02:26:06 2023    ] "
+    echo " [ %c : (JP) 水  6/21 02:26:06 2023    ] "
+    echo " [ %c : (US) Wed Jun 21 02:26:06 2023 ] "
+    echo
+    echo " locale Change : KR "
+    echo ' export LC_ALL="ko_KR.UTF-8" '
+    export LC_ALL="ko_KR.UTF-8"
+    echo ' date "+%c" '
     date "+%c"
+    echo ' date "+%a %b/%e %H:%M:%S %Y" '
+    date "+%a %b/%e %H:%M:%S %Y"
     echo
-    echo " - 날짜 형식 출력"
-    echo " 출력형식 종류(%F) : %Y-%m-%d 와 같은 형식"
+    echo
+    echo " locale Change : JP "
+    echo ' export LC_ALL="ja_JP.UTF-8" '
+    export LC_ALL="ja_JP.UTF-8"
+    echo ' date "+%c" '
+    date "+%c"
+    echo ' date "+%a %b/%e %H:%M:%S %Y" '
+    date "+%a %b/%e %H:%M:%S %Y"
+    echo
+    echo
+    echo " locale Change : US "
+    echo ' export LC_ALL="en_US.UTF-8" '
+    export LC_ALL="en_US.UTF-8"
+    echo ' date "+%c" '
+    date "+%c"
+    echo ' date "+%a %b %e %H:%M:%S %Y" '
+    date "+%a %b %e %H:%M:%S %Y"
+    echo
+    # Example 03 (Option : %F [YYYY-MM-DD] , %D [MM/DD/YY] , %x  [MM/DD/YYYY] )
+    echo 
+    echo " %F %D %x Type Print : LC_ALL Common"
+    echo " [ %F : YYYY-MM-DD ] "
+    echo " [ %D : MM/DD/YY   ] "
+    echo " [ %x : MM/DD/YYYY ] "
+    echo
+    echo ' date "+%F" '
     date "+%F"
-    echo " 출력형식 종류(%D) : %m/%d/%y 와 같은 형식"
-    date "+%D"
-    echo " 출력형식 종류(%x) : %m/%d/%Y 와 같은 형식"
-    date "+%x"
+    echo ' date "+%Y-%m-%d" '
+    date "+%Y-%m-%d"
     echo
-    echo " - 연도 형식 출력"
-    echo " 출력형식 종류(%Y) : 4자리 년도로 출력 ex)2022"
+    echo ' date "+%D" '
+    date "+%D"
+    echo ' date "+%m/%d/%y" '
+    date "+%m/%d/%y"
+    echo
+    echo ' date "+%x" '
+    date "+%x"
+    echo ' date "+%m/%d/%Y" '
+    date "+%m/%d/%Y"
+    echo
+    echo
+    # Example 04 (Option : %Y [YYYY] , %y ..[YY] , %C  [YY].. )
+    echo 
+    echo " %Y %y %C Type Print : LC_ALL Common"
+    echo " [ years ] "
+    echo " [ %Y : 2023 ] "
+    echo " [ %y :   23 ] "
+    echo " [ %C : 20   ] "
+    echo
+    echo ' date "+%Y" '
     date "+%Y"
-    echo " 출력형식 종류(%y) : 2자리 년도로 출력 ex)22"
+    echo ' date "+%y" '
     date "+%y"
-    echo " 출력형식 종류(%C) : 2022의 경우 앞의 두 자리 ex)20"
+    echo ' date "+%C" '
     date "+%C"
     echo
-    echo " - 월 형식 출력"
-    echo " 출력형식 종류(%m) : 월 출력(숫자) ex)11"
+    # Example 05 (Option : %m [2 words(Number)] , %B [locale Month] , %b [US locale 3 Words(Word)] , %h [Same %b] )
+    echo 
+    echo " [ months ] "
+    echo " [ %m : (KR) 06  (JP) 06  (US) 06    ] "
+    echo " [ %B : (KR) 6월  (JP) 6月 (US) June  ] "
+    echo " [ %b : (KR) 6   (JP) 6   (US) Jun   ] "
+    echo " [ %h : (KR) 6   (JP) 6   (US) Jun   ] "
+    echo
+    echo " locale Change : KR "
+    echo ' export LC_ALL="ko_KR.UTF-8" '
+    export LC_ALL="ko_KR.UTF-8"
+    echo ' date "+%m" '
     date "+%m"
-    echo " 출력형식 종류(%B) : 월 출력(영문시 풀네임 출력 January) ex)November"
+    echo ' date "+%B" '
     date "+%B"
-    echo " 출력형식 종류(%b) : 월 출력(영문시 약어 출력 Jan) ex)Nov"
+    echo ' date "+%b" '
     date "+%b"
-    echo " 출력형식 종류(%h) : 월 출력 %b와 같은 형식, 로케일에 영향없는 형식"
+    echo ' date "+%h" '
     date "+%h"
     echo
-    echo " - 일 형식 출력"
-    echo " 출력형식 종류(%d) : 일 출력 두 자리 출력 ex)01"
-    date "+%d"
-    echo " 출력형식 종류(%e) : 일 출력 한 자리 출력, %_d와 같은 의미 ex)1"
-    date "+%e"
-    date "+%_d"
+    echo " locale Change : JP "
+    echo ' export LC_ALL="ja_JP.UTF-8" '
+    export LC_ALL="ja_JP.UTF-8"
+    echo ' date "+%m" '
+    date "+%m"
+    echo ' date "+%B" '
+    date "+%B"
+    echo ' date "+%b" '
+    date "+%b"
+    echo ' date "+%h" '
+    date "+%h"
     echo
-    echo " - 요일 형식 출력"
-    echo " 출력형식 종류(%A) : 요일 출력(영문시 Sunday) ex)Sunday"
+    echo " locale Change : US "
+    echo ' export LC_ALL="en_US.UTF-8" '
+    export LC_ALL="en_US.UTF-8"
+    echo ' date "+%m" '
+    date "+%m"
+    echo ' date "+%B" '
+    date "+%B"
+    echo ' date "+%b" '
+    date "+%b"
+    echo ' date "+%h" '
+    date "+%h"
+    echo
+    # Example 06 (Option : %d [2 words(Number)] , %e [1 word(Number)] )
+    echo
+    echo " %d %e Type Print : LC_ALL Common"
+    echo " [ days ] "
+    echo " [ %d : 09         2Word ] "
+    echo " [ %e :  9 (= %_d) 1Word ] "
+    echo
+    echo ' date "+%d" '
+    date "+%d"
+    echo ' date "+%e" '
+    date "+%e"
+    echo ' date "+%_d" '
+    date "+%_d" 
+    echo
+    # Example 07 (Option : %A [Full Week] , %a [1 Word Week] , %u [1(Mon)~7(Sun)] ,  %w [0(Sun)~6(Mon)] )
+    echo 
+    echo " [ weeks ] "
+    echo " [ %A : (KR) 수요일 (JP) 水曜日 (US) Wednesday ] "
+    echo " [ %a : (KR) 수    (JP) 水    (US) Wed       ] "
+    echo " [ %u : (KR) 3    (JP) 3     (US) 3         ] : 1(Mon)~7(Sun) "
+    echo " [ %w : (KR) 3    (JP) 3     (US) 3         ] : 0(Sun)~6(Mon) "
+    echo
+    echo " locale Change : KR "
+    echo ' export LC_ALL="ko_KR.UTF-8" '
+    export LC_ALL="ko_KR.UTF-8"
+    echo ' date "+%A" '
     date "+%A"
-    echo " 출력형식 종류(%a) : 요일 출력(영문시 Sun) ex)Sun"
+    echo ' date "+%a" '
     date "+%a"
-    echo " 출력형식 종류(%u) : 요일 출력(숫자 1 : 월요일, 7 : 일요일)"
+    echo ' date "+%u" '
     date "+%u"
-    echo " 출력형식 종류(%w) : 요일 출력(숫자 0 : 일요일, 6 : 월요일)"
+    echo ' date "+%w" '
     date "+%w"
     echo
-    echo " - 시간 형식 출력"
-    echo " 출력형식 종류(%r) : 시간 출력 시분초(12시간 단위) ex)오전 x시x분x초"
-    date "+%r"
-    echo " 출력형식 종류(%X) : 시간 출력 시분초(24시간 단위) ex)x시x분x초"
-    date "+%X"
-    echo " 출력형식 종류(%T) : 시간 출력 시분초(24시간 단위), %H:%M:%S와 같은 의미 ex)x시x분x초"
-    date "+%T"
-    echo " 출력형식 종류(%R) : 시간 출력 시분(24시간 단위) ex)x시x분"
-    date "+%R"
+    echo " locale Change : JP "
+    echo ' export LC_ALL="ja_JP.UTF-8" '
+    export LC_ALL="ja_JP.UTF-8"
+    echo ' date "+%A" '
+    date "+%A"
+    echo ' date "+%a" '
+    date "+%a"
+    echo ' date "+%u" '
+    date "+%u"
+    echo ' date "+%w" '
+    date "+%w"
     echo
-    echo " - 오전오후 구분자 형식 출력"
-    echo " 출력형식 종류(%p) : 오전 오후 형식 출력 ex)AM"
+    echo " locale Change : US "
+    echo ' export LC_ALL="en_US.UTF-8" '
+    export LC_ALL="en_US.UTF-8"
+    echo ' date "+%A" '
+    date "+%A"
+    echo ' date "+%a" '
+    date "+%a"
+    echo ' date "+%u" '
+    date "+%u"
+    echo ' date "+%w" '
+    date "+%w"
+    echo
+    # Example 08 (Option : %p [AM/PM] )
+    echo
+    echo " %p Type Print : LC_ALL Common"
+    echo " [ %p : AM/PM ] "
+    echo ' date "+%p" '
     date "+%p"
     echo
-    echo " - 시 형식 출력"
-    echo " 출력형식 종류(%H) : 두 자리 시간 출력(24시간 단위) ex)05"
+    # Example 09 (Option : %H [2 words 24hours] , %I [2 words 12hours] , %k [1 word 24hours] , %l [1 word 12hours] )
+    echo 
+    echo " %H %I %k %l Type Print : LC_ALL Common"
+    echo " [ hours ] "
+    echo " [ %H : 02 (2 words 24hours)] "
+    echo " [ %I : 02 (2 words 12hours)] "
+    echo " [ %k :  2 (1 word  24hours)] "
+    echo " [ %l :  2 (1 word  12hours)] "
+    echo
+    echo ' date "+%H" '
     date "+%H"
-    echo " 출력형식 종류(%I) : 두 자리 시간 출력(12시간 단위) ex)05"
+    echo ' date "+%I" '
     date "+%I"
-    echo " 출력형식 종류(%k) : 한 자리 시간 출력(24시간 단위) ex)5"
+    echo ' date "+%k" '
     date "+%k"
-    echo " 출력형식 종류(%l) : 한 자리 시간 출력(12시간 단위) ex)05"
+    echo ' date "+%l" '
     date "+%l"
     echo
-    echo " - 분 형식 출력"
-    echo " 출력형식 종류(%M) : 두 자리 분 출력 ex)05"
+    # Example 10 (Option : %M [2 words minutes] )
+    echo 
+    echo " %M Type Print : LC_ALL Common"
+    echo " [ Minutes ] "
+    echo " [ %M : 02 (2 words)] "
+    echo " [ %_M : 2 (1 word)] "
+    echo
+    echo ' date "+%M" '
     date "+%M"
+    echo ' date "+%_M" '
+    date "+%_M"
     echo
-    echo " - 초 형식 출력"
-    echo " 출력형식 종류(%S) : 두 자리 초 출력 ex)05"
+    # Example 11 (Option : %S [2 words seconds] )
+    echo 
+    echo " %S Type Print : LC_ALL Common"
+    echo " [ Seconds ] "
+    echo " [ %S : 02 (2 words)] "
+    echo " [ %_S : 2 (1 word)] "
+    echo
+    echo ' date "+%S" '
     date "+%S"
+    echo ' date "+%_S" '
+    date "+%_S"
     echo
-    echo " - 지난 시간 계산 형식 출력"
-    echo " 출력형식 종류(%j) : 올해의 경과된 날짜 시간 출력"
-    date "+%j"
-    echo " 출력형식 종류(%s) : 1970/01/01 00:00:00로부터 경과한 초 출력"
-    date "+%s"
-    echo " 출력형식 종류(%U) : 일요일이 시작으로 몇 주 출력(00~53)"
-    date "+%U"
-    echo " 출력형식 종류(%V) : 월요일이 시작으로 몇 주 출력(ISO 주 번호 01~53)"
-    date "+%V"
-    echo " 출력형식 종류(%W) : 월요일이 시작으로 몇 주 출력(00~53)"
-    date "+%W"
+    # Example 12 (Option : %z [Timezone time] , %Z [Timezone area] )
+    echo 
+    echo " %z %Z Type Print : LC_ALL Common"
+    echo " [ Timezone ] "
+    echo " [ %z : +0900 (timezone time)] "
+    echo " [ %Z :   JST (timezone area)] "
     echo
-    echo " - 타임존 형식 출력"
-    echo " 출력형식 종류(%z) : 타임존 형식 출력 (시)"
+    echo ' date "+%z" '
     date "+%z"
-    echo " 출력형식 종류(%Z) : 타임존 형식 출력(시간 대역 이름)"
+    echo ' date "+%Z" '
     date "+%Z"
     echo
-    echo " - 명령어 커맨드 형식 출력"
-    echo " 출력형식 종류(%t) : 탭 출력"
+    # Example 13 (Option : %j [the date past this year] , %s , %U , %V , %W )
+    echo 
+    echo " %j %s %U %V %W Type Print : LC_ALL Common"
+    echo " [ Time Calculator ] "
+    echo " [ %j : 172        (the date past this year)    ] "
+    echo " [ %s : 1687284763 (the seconds past this year) ] "
+    echo " [ %U : 25         (week past this year(sunday start(00~53))) ] "
+    echo " [ %V : 25         (week past this year(monday start(10~53))) ] "
+    echo " [ %W : 25         (week past this year(monday start(00~53))) ] "
+    echo
+    echo ' date "+%j" '
+    date "+%j"
+    echo ' date "+%s" '
+    date "+%s"
+    echo ' date "+%U" '
+    date "+%U"
+    echo ' date "+%V" '
+    date "+%V"
+    echo ' date "+%W" '
+    date "+%W"
+    echo
+    # Example 14 (Option : %t [tap] , %n [new line] , %% [% print] )
+    echo
+    echo " %j %s %U %V %W Type Print : LC_ALL Common"
+    echo " [ Time Calculator ] "
+    echo " [ %t (tap)      ] "
+    echo " [ %n (new Line) ] "
+    echo " [ %% (% print)  ] "
+    echo
+    echo ' date "+%t" '
     date "+%t"
-    echo " 출력형식 종류(%n) : 새 줄 출력"
+    echo ' date "+%n" '
     date "+%n"
-    echo " 출력형식 종류(%%) : % 출력"
+    echo ' date "+%%" '
     date "+%%"
     echo
-    echo " 옵션 -u : 표준시간으로 출력하기 "
-    echo " 예시3) date -u"
+    # Example 15 (Option -u : Output in standard time [UTC Time])
+    echo
+    echo " date -u "
     date -u
     echo
-    echo " 옵션 -R : 날짜 + 시간 출력 "
-    echo " 예시3) date -R : %a, %d %b %Y %H:%M:%S %z 형식으로 출력(locale : C)"
-    date -R
+    # Example 15 (Option -R : Output in standard time [RFC2822 Format + UTC Time])
     echo
-    echo "## 현재 시간 출력 처리 종료 ##"
+    echo " date -R "
+    date -R
+    echo ' date "+%a, %d %b %Y %H:%M:%S %z" '
+    date "+%a, %d %b %Y %H:%M:%S %z"
+    echo
+    # Example End
+    echo
+    echo "## Date End ##"
     echo 
 }
 
-CurrentDate 
+Date_CurrentDate
