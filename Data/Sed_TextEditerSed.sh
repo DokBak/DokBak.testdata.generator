d1ce2450a13e584164990456371e58e9073d9d14
DokBak <photoismylife0216@gmail.com>
2023-07-05 02:08:07 +0900

    FIX : Sed_TextEditerSed.sh
    Not Option
    Option -n
    Option -e
    Option -f

--- Data/Sed_TextEditerSed.sh
+++ Data/Sed_TextEditerSed.sh
@@ -1,81 +1,124 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 비대화형 모드의 텍스트 파일 에디터
-#-------------------------
-function TextEditerSed(){
-    echo 
-    echo "## 비대화형 모드의 텍스트 파일 에디터 시작 ##"
-    echo " 명령어  : sed"
-    echo " 사용방법 : sed [옵션] [연산자] [검색대상]"
-    echo " 기본설명 : sed 스트림 에디터는 한 번에 하나의 입력 또는 파일으로부터 한 줄만 처리하고 출력한다. 처리할 라인을 sed임시 버퍼에 저장후 임시 버퍼에서 처리후 출력된다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Sed]_[TextEditerSed]
+#------------------------------------------------------------
+function sed_TextEditerSed(){
     echo
-    echo " 사전실행) sed 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir -p sed_CMD_TestFolder/"
-    mkdir -p sed_CMD_TestFolder/
-    echo "    echo 'TestFile1' > sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'apple,banana,cream,dust,Test,txt,TestFile1,gui' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'Test,bash,zsh,sh' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'Test,java,Javascript,C,C#,C++,python,ruby' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'TestFile2' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'iphone,ipad,imac,Test,ipod,ipodtouch' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'iphone,ipod,ipodtouch,Test,ipad,imac' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'galaxyS7,Note7,ZFilp,S7,Test' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'TestFile3' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'Korea,Test,Japan,China' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'Seoul,Tokyo,hongkong' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo '0102,12310,asdas' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'TestFile4' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'ps1,ps2,ps3,ps4' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'psa,psa,psa,psa' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo "    echo 'Nintendo,NDSL,DSL,wii' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt"    
-    echo 'TestFile1' > sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'apple,banana,cream,dust,Test,txt,TestFile1,gui' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'Test,bash,zsh,sh' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'Test,java,Javascript,C,C#,C++,python,ruby' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'TestFile2' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'iphone,ipad,imac,Test,ipod,ipodtouch' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'iphone,ipod,ipodtouch,Test,ipad,imac' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'galaxyS7,Note7,ZFilp,S7,Test' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'TestFile3' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'Korea,Test,Japan,China' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'Seoul,Tokyo,hongkong' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo '0102,12310,asdas' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'TestFile4' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'ps1,ps2,ps3,ps4' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'psa,psa,psa,psa' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt
-    echo 'Nintendo,NDSL,DSL,wii' >> sed_CMD_TestFolder/sed_CMD_TestFile1.txt   
+    echo "## sed Start ##"
     echo
-    cat sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    # Basic Information Start
     echo
-    echo " ###sed 연산자 종류###"
-    echo " [주소 범위]/p : print : [주어진 주소 범위]를 출력한다."
-    echo " [주소 범위]/d : delete : [주어진 주소 범위]를 삭제한다." 
-    echo " s/pattern1/pattern2/ : substitute : 한 라인에서 처음 나타나는 patttern1을 pattern2로 치환한다." 
-    echo " [주소 범위]/s/pattern1/pattern2/ : substitute : 주소 범위에 대해서 한 라인에 처음 나타나는 pattern1을 pattern2로 치환한다." 
-    echo " [주소 범위]/y/pattern1/pattern2/ : transform : 주소 범위에 대해서 pattern1에 나타나는 어떤 문자라도 pattern2에 나타나는 문자로 변경한다. (tr과 동일)" 
-    echo " g : global : 모든 라인에서 입력과 일치하는 패턴에 대해 동작한다." 
-    echo " ###sed 연산자 종류###"
+    echo " Command  : sed"
+    echo " HowToUse : sed [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : Pattern "
+    echo "            [Argument2] : tarFile_Path "
+    echo "            [Option : -n] : Only Correct Pattern Print "
+    echo "            [Option : -e] : Script Set "
+    echo "            [Option : -f] : Script File Read "
     echo
-    echo " 예시1) sed -n '2,6p' sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo " 옵션 -n : 옵션을 사용하면 패턴이 일치하는 라인만 출력한다. 이 옵션이 없다면 모든 입력이 출력된다. (-n을 사용시 /p도 같이 사용한다.)"
-    echo " 부가설명) sed_CMD_TestFolder/sed_CMD_TestFile1.txt파일에서 2번째줄부터 6번째줄까지 출력(p)한다."
-    sed -n '2,6p' sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    # Basic Information End
+    # Preparation Start
     echo
-    echo " 예시2) sed '2,6d' sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo " 부가설명) sed_CMD_TestFolder/sed_CMD_TestFile1.txt파일에서 2번째줄부터 6번째줄까지 삭제(d)한다."
-    sed '2,6d' sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    # Preparation : mkdir
+    mkdir -p `pwd`/sed_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'TestFile1' > `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'TestFile1' > `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'apple,banana,cream,dust,Test,txt,TestFile1,gui' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'apple,banana,cream,dust,Test,txt,TestFile1,gui' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'Test,bash,zsh,sh' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'Test,bash,zsh,sh' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'Test,java,Javascript,C,C#,C++,python,ruby' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'Test,java,Javascript,C,C#,C++,python,ruby' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'TestFile2' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'TestFile2' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'iphone,ipad,imac,Test,ipod,ipodtouch' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'iphone,ipad,imac,Test,ipod,ipodtouch' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'iphone,ipod,ipodtouch,Test,ipad,imac' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'iphone,ipod,ipodtouch,Test,ipad,imac' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'galaxyS7,Note7,ZFilp,S7,Test' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'galaxyS7,Note7,ZFilp,S7,Test' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'TestFile3' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'TestFile3' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'Korea,Test,Japan,China' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'Korea,Test,Japan,China' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'Seoul,Tokyo,hongkong' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'Seoul,Tokyo,hongkong' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo '0102,12310,asdas' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo '0102,12310,asdas' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'TestFile4' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'TestFile4' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'ps1,ps2,ps3,ps4' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'ps1,ps2,ps3,ps4' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'psa,psa,psa,psa' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo 'psa,psa,psa,psa' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 'Nintendo,NDSL,DSL,wii' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "    
+    echo 'Nintendo,NDSL,DSL,wii' >> `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
     echo
-    echo " 예시3) sed -n 's/ip/sam/p' sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo " 부가설명) sed_CMD_TestFolder/sed_CMD_TestFile1.txt파일에서 각 라인에서 가장 빠르게 검색되는 'ip'를 'sam'으로 변경하여 출력한다,"
-    sed -n 's/ip/sam/p' sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo " echo 's/TestFile/fileRead/g' > `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile2.txt "
+    echo 's/TestFile/fileRead/g' > `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile2.txt
     echo
-    echo " 예시4) sed 's/ip/sam/g' sed_CMD_TestFolder/sed_CMD_TestFile1.txt"
-    echo " 부가설명) sed_CMD_TestFolder/sed_CMD_TestFile1.txt파일에서 각 라인에 존재하는 모든 'ip'를 'sam'으로 변경하여 출력한다,"
-    sed 's/ip/sam/g' sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : ['s(tart),e(nd)'d] )
     echo
-    echo "## 특정 비대화형 모드의 텍스트 파일 에디터 종료 ##"
-    echo 
+    echo " Select Line Delete : ['s(tart),e(nd)'d] "
+    echo " sed '2,5d' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo " sed '2,5'd `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed '2,5d' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 02 (Not Option : ['s(tart),e(nd)'p] )
+    echo
+    echo " Select Line Print : ['s(tart),e(nd)'d] "
+    echo " sed '2,5p' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    echo " sed '2,5'p `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed '2,5'p `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 03 (Not Option : ['s/pattern1/pattern2/] )
+    echo
+    echo " Select Pattern One Time Change : ['s/pattern1/pattern2/'] "
+    echo " sed 's/TestFile/SedSample/' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed 's/TestFile/SedSample/' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 04 (Not Option : ['s(tart),e(nd)s/pattern1/pattern2/] )
+    echo
+    echo " Select Pattern Change One Time (on 2~5Line): ['s(tart),e(nd)s/pattern1/pattern2/'] "
+    echo " sed '2,5s/TestFile/SedSample/' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed '2,5s/TestFile/SedSample/' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 05 (Not Option : ['s/pattern1/pattern2/] )
+    echo
+    echo " Select Pattern All Change : ['s/pattern1/pattern2/g'] "
+    echo " sed 's/TestFile/SedSample/g' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed 's/TestFile/SedSample/g' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 06 (Not Option -n : Only Correct Pattern Print )
+    echo
+    echo " Only Correct Pattern Print : [ -n 's(tart),e(nd)p'] "
+    echo " sed -n '2,5p' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed -n '2,5p' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 07 (Not Option -n : Only Correct Pattern Print )
+    echo
+    echo " Only Correct Pattern Print : [ -n 's/pattern1/pattern2/p'] "
+    echo " sed -n 's/TestFile/SedSample/p' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed -n 's/TestFile/SedSample/p' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 08 (Not Option -e : Script Set)
+    echo
+    echo " Script Set : [ -e 's/pattern1/pattern2/g'] "
+    echo " sed -e 's/TestFile/SedSample/g' -e 's/SedSample/tt/g' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed -e 's/TestFile/SedSample/g' -e 's/SedSample/tt/g' `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    # Example 09 (Not Option -f : Script File Read)
+    echo
+    echo " Script Set : [ -f 's/pattern1/pattern2/g'] "
+    echo " sed -f `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile2.txt `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt "
+    sed -f `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile2.txt `pwd`/sed_CMD_TestFolder/sed_CMD_TestFile1.txt
+    echo
+    echo "## sed End ##"
+    echo
 }
 
-TextEditerSed
+sed_TextEditerSed
