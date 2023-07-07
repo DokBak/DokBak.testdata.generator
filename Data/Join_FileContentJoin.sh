0a8305f4a8ea703303664d978cee0bc3710b0b9d
DokBak <photoismylife0216@gmail.com>
2023-07-08 00:20:18 +0900

    FIX : Join_FileContentJoin.sh
    Not Option
    Option -o
    Option -t

--- Data/Join_FileContentJoin.sh
+++ Data/Join_FileContentJoin.sh
@@ -1,36 +1,73 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 파일 내용 조인
-#-------------------------
-function FileContentJoin(){
-    echo 
-    echo "## 파일 내용 조인 시작 ##"
+#------------------------------------------------------------
+# Cmd test Shell : [Join]_[FileContentJoin]
+#------------------------------------------------------------
+function join_FileContentJoin(){
     echo
-    echo " 명령어  : join"
-    echo " 사용방법 : join [파일명1] [파일명2] "
-    echo " 기본설명 : 두 파일의 값중 같은값을 가지는 필드의 쌍이 발견되면 그 필드를 하나로 합치고 나머지 필드는 가로로 합쳐져서 출력된다.(디폴트 : 크로스조인)"
+    echo "## join Start ##"
     echo
-    echo " 사전실행) join 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir join_CMD_TestFolder/"
-    mkdir -p join_CMD_TestFolder
-    echo 'test apple' > join_CMD_TestFolder/join_CMD_TestFile1.txt
-    echo 'test banana' >> join_CMD_TestFolder/join_CMD_TestFile1.txt
-    echo 'test melon' >> join_CMD_TestFolder/join_CMD_TestFile1.txt
-    echo 'test pear' >> join_CMD_TestFolder/join_CMD_TestFile1.txt
-    echo 'test peach' >> join_CMD_TestFolder/join_CMD_TestFile1.txt
+    # Basic Information Start
+    echo
+    echo " Command  : join"
+    echo " HowToUse : join [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : tarFile_Path1 "
+    echo "            [Argument2] : tarFile_Path2 "
+    echo "            [Option : -o] : [Argument1.column],[Argument2.column] select column Join "
+    echo "            [Option : -t] : [delimiters] Join "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/join_CMD_TestFolder/ "
+    mkdir -p `pwd`/join_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'test apple A' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt "
+    echo 'test apple A' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt
+    echo " echo 'test banana B' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt "
+    echo 'test banana B' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt
+    echo " echo 'test melon C' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt "
+    echo 'test melon C' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt
+    echo
+    echo " echo 'test,pear,D' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile3.txt "
+    echo 'test,pear,D' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile3.txt
+    echo " echo 'test,peach,E' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile3.txt "
+    echo 'test,peach,E' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile3.txt
     echo 
-    echo 'test 300' > join_CMD_TestFolder/join_CMD_TestFile2.txt
-    echo 'test 350' >> join_CMD_TestFolder/join_CMD_TestFile2.txt
-    echo 'test 120' >> join_CMD_TestFolder/join_CMD_TestFile2.txt
-    echo 'test 270' >> join_CMD_TestFolder/join_CMD_TestFile2.txt
-    echo 'test 410' >> join_CMD_TestFolder/join_CMD_TestFile2.txt
-    echo 
-    echo " 예시1) join join_CMD_TestFolder/join_CMD_TestFile1.txt join_CMD_TestFolder/join_CMD_TestFile2.txt"
-    join join_CMD_TestFolder/join_CMD_TestFile1.txt join_CMD_TestFolder/join_CMD_TestFile2.txt
-    echo 
-    echo "## 파일 내용 조인 종료 ##"
-    echo 
+    echo " echo 'test 300 F' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt "
+    echo 'test 300 F' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt
+    echo " echo 'test 350 G' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt "
+    echo 'test 350 G' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt
+    echo
+    echo " echo 'test,120,X' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt "
+    echo 'test,120,X' > `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt
+    echo " echo 'test,270,Y' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt "
+    echo 'test,270,Y' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt
+    echo " echo 'test,410,Z' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt "
+    echo 'test,410,Z' >> `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : cross Join)
+    echo
+    echo " join `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt "
+    join `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt
+    echo
+    # Example 02 (Option -o : [Argument1.column],[Argument2.column] select column Join)
+    echo
+    echo " join -o 1.2,2.2 `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt "
+    join -o 1.2,2.2 `pwd`/join_CMD_TestFolder/join_CMD_TestFile1.txt `pwd`/join_CMD_TestFolder/join_CMD_TestFile2.txt
+    echo
+    # Example 03 (Option -t : [delimiters] Join )
+    echo
+    echo " join -t,  `pwd`/join_CMD_TestFolder/join_CMD_TestFile3.txt `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt "
+    join -t,  `pwd`/join_CMD_TestFolder/join_CMD_TestFile3.txt `pwd`/join_CMD_TestFolder/join_CMD_TestFile4.txt
+    echo
+    # Example End
+    echo
+    echo "## join End ##"
+    echo
 }
 
-FileContentJoin
+join_FileContentJoin
