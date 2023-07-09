cb1318a728a121876153dea86394c6b6a8fe08eb
DokBak <photoismylife0216@gmail.com>
2023-07-10 04:04:42 +0900

    FIX : Df_DiskSpaceCheck.sh
    Not Option
    Option -h
    Option -H
    Option -l
    Option -P

--- System/Df_DiskSpaceCheck.sh
+++ System/Df_DiskSpaceCheck.sh
@@ -1,42 +1,5 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 디스크 사용량 체크
-#-------------------------
-function DiskSpaceCheck(){
-    echo 
-    echo "## 디스크 사용량 체크 시작 ##"
-    echo " 명령어  : df"
-    echo " 사용방법 : df [옵션]"
-    echo " 기본설명 : 리눅스 시스템 전체의 마운트 된 디스크 사용량을 확인할 수 있다."
-    echo
-    echo " 예시1) df"
-    echo " 부가설명) 파일 시스템, 디스크 크기, 사용량, 여유공간, 사용률, 마운트지점 순으로 출력된다. 옵션이 없는 경우 kB(킬로바이트) 단위로 출력"
-    df
-    echo " 옵션 -h : 사람이 읽을 수 있는 단위중 읽기 쉬운 단위로 출력 (1024단위로 용량 표기)"
-    echo " 예시2) df -h"
-    df -h
-    echo
-    echo " 옵션 -H : 사람이 읽을 수 있는 단위중 읽기 쉬운 단위로 출력 (1000단위로 용량 표기)"
-    echo " 예시2) df -H"
-    df -H
-    echo
-    echo " 옵션 -l : 로컬 파일 시스템만 출력(외장 메모리등은 제외)"
-    echo " 예시2) df -l"
-    df -l
-    echo
-    echo " 옵션 -p : 파일 시스템 경로가 긴 경우 2줄로 출력되는 경우가 있으나, -P 옵션으로 한줄로 출력"
-    echo " 예시2) df -P"
-    df -P
-    echo
-    echo "## 디스크 사용량 체크 종료 ##"
-    echo 
-}
-
-DiskSpaceCheck
-
-#!/bin/sh
-
 #------------------------------------------------------------
 # Cmd Test Shell : [Df]_[DiskSpaceCheck]
 #------------------------------------------------------------
@@ -67,21 +30,25 @@ function df_DiskSpaceCheck(){
     echo 
     # Example 02 (Option -h : Human-readable output /1024 )
     echo
+    echo " Option -h : Human-readable output /1024 "
     echo " df -h "
     df -h 
     echo
     # Example 03 (Option -H : Human-readable output /1000 )
     echo
+    echo " Option -H : Human-readable output /1000 "
     echo " df -H "
     df -H 
     echo
     # Example 04 (Option -l : Only display information about locally-mounted)
     echo
+    echo " Option -l : Only display information about locally-mounted "
     echo " df -l "
     df -l 
     echo
     # Example 05 (Option -P : Path Line 2 -> 1)
     echo
+    echo " Option -P : Path Line 2 -> 1 "
     echo " df -P "
     df -P 
     echo
