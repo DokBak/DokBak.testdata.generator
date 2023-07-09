1a3f335156daa5c7215e22be585413464d6698ae
DokBak <photoismylife0216@gmail.com>
2023-07-10 04:57:28 +0900

    FIX : Ping_NetworkConnecting.sh
    Not Option
    Option -c
    Option -i
    Option -t

--- System/Ping_NetworkConnecting.sh
+++ System/Ping_NetworkConnecting.sh
@@ -22,23 +22,26 @@ function ping_NetworkConnecting(){
     echo
     # Preparation End
     # Example Start
-    # Example 01 (Not Option)
+    # Example 01 (Not Option : Packet Transmissions check )
     echo
     echo " ping google.com"
     ping google.com
     echo
-    # Example 02 (Option -c : Number of Packet Transmissions)
+    # Example 02 (Option -c : Number of Packet Transmissions )
     echo
+    echo " Option -c : Number of Packet Transmissions "
     echo " ping google.com -c 3"
     ping google.com -c 3
     echo
-    # Example 03 (Option -i : Packet Transmissions Interval(Seconds))
+    # Example 03 (Option -i : Packet Transmissions Interval(Seconds) )
     echo
+    echo " Option -i : Packet Transmissions Interval(Seconds) "
     echo " ping google.com -i 3"
     ping google.com -i 3
     echo
-    # Example 04 (Option -t : The Maximum Time To Wait For a Response)
+    # Example 04 (Option -t : The Maximum Time To Wait For a Response )
     echo
+    echo " Option -t : The Maximum Time To Wait For a Response "
     echo " ping google.com -t 2"
     ping google.com -t 2
     echo
