b78255cf0112861e20ead4d6f924a71471217360
DokBak <photoismylife0216@gmail.com>
2023-07-10 04:51:16 +0900

    FIX : Du_DirectoryUsageCheck.sh
    Not Option
    Option -a
    Option -h
    Option -s

--- System/Du_DirectoryUsageCheck.sh
+++ System/Du_DirectoryUsageCheck.sh
@@ -12,9 +12,9 @@ function du_DirectoryUsageCheck(){
     echo " Command  : du"
     echo " HowToUse : du [Option] [Argument1] "
     echo "            [Argument1] : Directory "
-    echo "            [Option : -a] : Check up to Sub Directories) "
-    echo "            [Option : -h] : “Human-readable” output) "
-    echo "            [Option : -s] : Total Usage Sum Directory) "
+    echo "            [Option : -a] : Check up to Sub Directories "
+    echo "            [Option : -h] : “Human-readable” output "
+    echo "            [Option : -s] : Total Usage Sum Directory "
     echo
     # Basic Information End
     # Preparation Start
@@ -22,28 +22,33 @@ function du_DirectoryUsageCheck(){
     echo
     # Preparation End
     # Example Start
-    # Example 01 (Not Option)
+    # Example 01 (Not Option : Check the size of all files )
     echo
+    echo " Not Option : Check the size of all files "
     echo " du "
     du
     echo 
-    # Example 02 (Option)
+    # Example 02 (Not Option : Check the size of current directory )
     echo
+    echo " Not Option : Check the size of current directory "
     echo " du `pwd`/ "
     du `pwd`/
     echo
-    # Example 03 (Option -a : Check up to Sub Directories)
+    # Example 03 (Option -a : Check up to Sub Directories )
     echo
+    echo " Option -a : Check up to Sub Directories "
     echo " du -a `pwd`/ "
     du -a `pwd`/
     echo
-    # Example 04 (Option -h : Human-readable output)
+    # Example 04 (Option -h : Human-readable output )
     echo
+    echo " Option -h : Human-readable output "
     echo " du -h `pwd`/ "
     du -h `pwd`/
     echo
-    # Example 05 (Option -s : Total Usage Sum Directory)
+    # Example 05 (Option -s : Total Usage Sum Directory )
     echo
+    echo " Option -s : Total Usage Sum Directory "
     echo " du -s `pwd`/ "
     du -s `pwd`/
     echo
