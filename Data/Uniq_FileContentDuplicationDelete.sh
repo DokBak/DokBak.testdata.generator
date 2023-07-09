3cb2210fefe0d806940d9eeee4598c7550419dfd
DokBak <photoismylife0216@gmail.com>
2023-07-10 05:31:58 +0900

    FIX : Uniq_FileContentDuplicationDelete.sh
    Not Option
    Option -c
    Option -i

--- Data/Uniq_FileContentDuplicationDelete.sh
+++ Data/Uniq_FileContentDuplicationDelete.sh
@@ -59,19 +59,19 @@ function uniq_FileContentDuplicationDelete(){
     echo
     # Preparation End
     # Example Start
-    # Example 01 (Not Option)
+    # Example 01 (Not Option : Remove consecutive duplicate values )
     echo
     cat `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt
     echo
     echo " uniq `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt "
     uniq `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt
     echo
-    # Example 02 (Option -c : duplicate values print)
+    # Example 02 (Option -c : duplicate values print )
     echo
     echo " uniq -c `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt "
     uniq -c `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt
     echo
-    # Example 03 (Option -i : Not case sensitive)
+    # Example 03 (Option -i : Not case sensitive )
     echo
     echo " uniq -i `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt "
     uniq -i `pwd`/uniq_CMD_TestFolder/uniq_CMD_TestFile1.txt
