6e0ebb3ad9f05fb50652944cfe0e8254eeb1b1ca
DokBak <photoismylife0216@gmail.com>
2023-07-10 04:58:45 +0900

    FIX : Set_LocalEnvironmentalVariable.sh
    Not Option

--- System/Set_LocalEnvironmentalVariable.sh
+++ System/Set_LocalEnvironmentalVariable.sh
@@ -18,13 +18,15 @@ function set_LocalEnvironmentalVariable(){
     echo
     # Preparation End
     # Example Start
-    # Example 01 (Not Option)
+    # Example 01 (Not Option : Environment variable output )
     echo
+    echo " Not Option : Environment variable output "
     echo " set "
     set
     echo
     # Example 02 (Not Option : grep ENV_PARAM )
     echo
+    echo " Not Option : grep ENV_PARAM "
     echo " env | grep ENV_PARAM "
     env | grep ENV_PARAM
     echo " set | grep ENV_PARAM "
@@ -32,6 +34,7 @@ function set_LocalEnvironmentalVariable(){
     echo
     # Example 03 (Not Option : grep ENV_PARAM -> Local_ENV )
     echo
+    echo " Not Option : grep ENV_PARAM -> Local_ENV "
     echo " ENV_PARAM=Local_ENV "
     ENV_PARAM=Local_ENV
     echo " env | grep ENV_PARAM "
@@ -39,8 +42,9 @@ function set_LocalEnvironmentalVariable(){
     echo " set | grep ENV_PARAM "
     set | grep ENV_PARAM
     echo
-    # Example 03 (Not Option : grep ENV_PARAM -> Global_ENV )
+    # Example 04 (Not Option : grep ENV_PARAM -> Global_ENV )
     echo
+    echo " Not Option : grep ENV_PARAM -> Global_ENV  "
     echo " export ENV_PARAM=Global_ENV "
     export ENV_PARAM=Global_ENV
     echo " env | grep ENV_PARAM "
