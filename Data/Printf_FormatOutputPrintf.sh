666253f6c441ad1cd717f446dbabca2727e662b8
DokBak <photoismylife0216@gmail.com>
2023-07-10 05:57:11 +0900

    FIX : Printf_FormatOutputPrintf.sh
    Not Option
    Option -v

--- Data/Printf_FormatOutputPrintf.sh
+++ Data/Printf_FormatOutputPrintf.sh
@@ -21,23 +21,27 @@ function printf_FormatOutputprintf(){
     echo
     # Preparation End
     # Example Start
-    # Example 01 (Not Option : %s [String], %f [Float], %d [Int])
+    # Example 01 (Not Option : %s [String], %f [Float], %d [Int] )
     echo
+    echo " Not Option : %s [String], %f [Float], %d [Int] "
     echo " printf '_%s_%f_%d_' 'test data' 15.1 15 "
     printf '_%s_%f_%d_' 'test data' 15.1 15
     echo
-    # Example 02 (Not Option : %[-n]s [ Right blank ], %[+n]s [ Left blank ])
+    # Example 02 (Not Option : %[-n]s [ Right blank ], %[+n]s [ Left blank ] )
     echo
+    echo " Not Option : %[-n]s [ Right blank ], %[+n]s [ Left blank ] "
     echo " printf '_%-15s_%11s_' 'test_data' 'data_test' "
     printf '_%-15s_%11s_' 'test_data' 'data_test'
     echo
-    # Example 03 (Not Option : %[n]d [ Left blank ], %0[n]d [ Left 0 ], %[n.m]f [ Int n, Float m ])
+    # Example 03 (Not Option : %[n]d [ Left blank ], %0[n]d [ Left 0 ], %[n.m]f [ Int n, Float m ] )
     echo 
+    echo " Not Option : %[n]d [ Left blank ], %0[n]d [ Left 0 ], %[n.m]f [ Int n, Float m ] "
     echo " printf '_%3d_%03d_%f_%.f_%.2f_%4.f_%6.2f_' 15 15 15.0123456 15.0123456 15.0123456 15.0123456 15.0123456 "
     printf '_%3d_%03d_%f_%.f_%.2f_%4.f_%6.2f_' 15 15 15.0123456 15.0123456 15.0123456 15.0123456 15.0123456
     echo
-    # Example 04 (Not Option : \" ["], \\ [\], \a [BeapSound], \b [BackSpace], \f [Formed], \n [NewLine], \r [], \r [CarriageReturn], \t [Tap], \t [HorizonTap], \v [VerticalTap], \c [NextCharNotPrint])
+    # Example 04 (Not Option : \" ["], \\ [\], \a [BeapSound], \b [BackSpace], \f [Formed], \n [NewLine], \r [], \r [CarriageReturn], \t [Tap], \t [HorizonTap], \v [VerticalTap], \c [NextCharNotPrint] )
     echo
+    echo " EXCAPE String "
     echo " printf 'S\"E' "
     printf 'S\"E'
     echo
@@ -68,8 +72,9 @@ function printf_FormatOutputprintf(){
     echo " printf 'S\cE' "
     printf 'S\cE'
     echo
-    # Example 05 (Not Option -v : Variable Set)
+    # Example 05 (Not Option -v : Variable Set )
     echo
+    echo " Not Option -v : Variable Set "
     echo "testVariable : ${testVariable}"
     echo " printf -v testVariable 30"
     printf -v testVariable 30
