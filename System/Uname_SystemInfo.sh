ba0e5139767085b7a66d1e43fd0839f542d5a001
DokBak <photoismylife0216@gmail.com>
2023-07-10 05:00:18 +0900

    FIX : Uname_SystemInfo.sh
    Not Option
    Option -s
    Option -n
    Option -r
    Option -v
    Option -m
    Option -p
    Option -a

--- System/Uname_SystemInfo.sh
+++ System/Uname_SystemInfo.sh
@@ -25,43 +25,51 @@ function uname_SystemInfo(){
     echo
     # Preparation End
     # Example Start
-    # Example 01 (Not Option)
+    # Example 01 (Not Option : Kernel Name )
     echo
+    echo " Not Option : Kernel Name "
     echo " uname "
     uname
     echo
-    # Example 02 (Option -s : Kernel Name )
+    # Example 02 (Option -s : Kernel Name[Default] )
     echo
+    echo " Option -s : Kernel Name[Default] "
     echo " uname -s "
     uname -s
     echo
     # Example 03 (Option -n : Network Host Name )
-    echo    
+    echo
+    echo " Option -n : Network Host Name "
     echo " uname -n "
     uname -n
     echo    
     # Example 04 (Option -r : Kernel Release Version )
     echo
+    echo " Option -r : Kernel Release Version "
     echo " uname -r "
     uname -r
     echo    
     # Example 05 (Option -v : Kernel Version )
     echo
+    echo " Option -v : Kernel Version "
     echo " uname -v "
     uname -v
     echo    
     # Example 06 (Option -m : System Hardware Type Info )
     echo
+    echo " Option -m : System Hardware Type Info "
     echo " uname -m "
     uname -m
     echo    
     # Example 07 (Option -p : Processor Type )
     echo
+    echo " Option -p : Processor Type "
     echo " uname -p "
     uname -p
     echo    
     # Example 08 (Option -a : All Info (-s -n -r -v -m) )
-    echo    
+    echo
+    echo " Option -a : All Info (-s -n -r -v -m) "
     echo " uname -a "
     uname -a
     uname -snrvm
