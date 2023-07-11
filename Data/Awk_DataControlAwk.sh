b80fde11b0b9f5b37dcfb6a15b52be1917be57f8
DokBak <photoismylife0216@gmail.com>
2023-07-12 00:36:50 +0900

    FIX : Awk_DataControlAwk.sh
    Not Option
    Option -f
    Option -F

--- Data/Awk_DataControlAwk.sh
+++ Data/Awk_DataControlAwk.sh
@@ -1,97 +1,142 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 데이터 컨트롤(awk)
-#-------------------------
-function DataControlAwk(){
-    echo 
-    echo "## 데이터 컨트롤(awk) 시작 ##"
-    echo " 명령어  : awk"
-    echo " 사용방법 : awk [옵션] ['pattern' or {action}]"
-    echo " 기본설명 : 데이터를 컨트롤 pattern 의 경우 if문의 조건과 같은 역할을 하고, action의 경우 {}로 감싸여진 문자으로 내부적으론 ;(세미콜론)으로 구분된다. "
+#------------------------------------------------------------
+# Cmd Test Shell : [Awk]_[DataControlAwk]
+#------------------------------------------------------------
+function awk_DataControlAwk(){
     echo
-    echo " 사전실행) awk 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir -p awk_CMD_TestFolder/"
-    mkdir -p awk_CMD_TestFolder/
-    echo "    echo 'Kr Korea 3324 5/11/96 50354' > awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo "    echo 'Jp Japan 5246 15/9/66 287650' >> awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo "    echo 'Us USA 7654 6/20/58 60000' >> awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo "    echo 'Cn China 8683 9/40/48 465000' >> awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo 'Kr Korea 3324 5/11/96 50354' > awk_CMD_TestFolder/awk_CMD_TestFile1.txt
-    echo 'Jp Japan 5246 15/9/66 287650' >> awk_CMD_TestFolder/awk_CMD_TestFile1.txt
-    echo 'Us USA 7654 6/20/58 60000' >> awk_CMD_TestFolder/awk_CMD_TestFile1.txt
-    echo 'Cn China 8683 9/40/48 465000' >> awk_CMD_TestFolder/awk_CMD_TestFile1.txt
-    echo "    echo '{print \"Country : \" \$1, \$2 }' > awk_CMD_TestFolder/awk_CMD_Command.txt"
-    echo "    echo '{print \$1, \$2, \$3, \$4, \$5 }' >> awk_CMD_TestFolder/awk_CMD_Command.txt"
-    echo '{print "Country : " $1, $2 }' > awk_CMD_TestFolder/awk_CMD_Command.txt
-    echo '{print $1, $2, $3, $4, $5 }' >> awk_CMD_TestFolder/awk_CMD_Command.txt
-    echo "    echo 'Kr Korea :3324 5/11/96 :50354' > awk_CMD_TestFolder/awk_CMD_TestFile2.txt"
-    echo "    echo 'Jp Japan :5246 15/9/66 :287650' >> awk_CMD_TestFolder/awk_CMD_TestFile2.txt"
-    echo "    echo 'Us USA :7654 6/20/58 :60000' >> awk_CMD_TestFolder/awk_CMD_TestFile2.txt"
-    echo "    echo 'Cn China :8683 9/40/48 :465000' >> awk_CMD_TestFolder/awk_CMD_TestFile2.txt"
-    echo 'Kr    Korea   :3324    :5/11/96    :50354' > awk_CMD_TestFolder/awk_CMD_TestFile2.txt
-    echo 'Jp    Japan    :5246    :15/9/66    :287650' >> awk_CMD_TestFolder/awk_CMD_TestFile2.txt
-    echo 'Us    USA    :7654    :6/20/58    :60000' >> awk_CMD_TestFolder/awk_CMD_TestFile2.txt
-    echo 'Cn    China    :8683    :9/40/48    :465000' >> awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo "## awk Start ##"
     echo
-    echo " 예시1) awk '/Korea/' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) [ awk 'pattern' filename ] file에서 'pattern'이 일치하는 데이터를 출력한다."
-    awk '/Korea/' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    # Basic Information Start
     echo
-    echo " 예시2) awk '{print \$2}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) [ awk '{action}' filename ] file에서 '{action}'과 일치하는 데이터(2번 항목에 해당하는 데이터)를 출력한다."
-    echo "         {action}에 \$0을 지정하면 모든 레코드를 대상으로 한다. \$0을 생략해도 디폴트로 모든 레코드를 대상으로 한다."
-    awk '{print $2}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo " Command  : awk"
+    echo " HowToUse : awk [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : Pattern "
+    echo "            [Argument2] : tarFile_Path "
+    echo "            [Option : -f] : awk Script File Read "
+    echo "            [Option : -F] : Specifying Separater "
     echo
-    echo "모든 레코드를 대상 \$0 지정 -> awk '{print \$0}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    awk '{print $0}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    # Basic Information End
+    # Preparation Start
     echo
-    echo "모든 레코드를 대상 \$0 미지정 -> awk '{print}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    awk '{print}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/awk_CMD_TestFolder/ "
+    mkdir -p `pwd`/awk_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'Kr Korea 3324 5/11/96 50354' > `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    echo " echo 'Jp Japan 5246 15/9/66 287650' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    echo " echo 'Us USA 7654 6/20/58 60000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    echo " echo 'Cn China 8683 9/40/48 465000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    echo 'Kr Korea 3324 5/11/96 50354' > `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo 'Jp Japan 5246 15/9/66 287650' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo 'Us USA 7654 6/20/58 60000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo 'Cn China 8683 9/40/48 465000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
     echo
-    echo " 예시3) awk '/Japan/{print \$3, \$4}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) [ awk 'pattern {action}' filename ] file에서 'pattern'이 일치하는 데이터중 3번째 4번째 항목 데이터를 출력한다."
-    awk '/Japan/{print $3, $4}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo " echo '{print \"Country : \" \$1, \$2 }' > `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt "
+    echo " echo '{print \$1, \$2, \$3, \$4, \$5 }' >> `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt "
+    echo '{print "Country : " $1, $2 }' > `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt
+    echo '{print $1, $2, $3, $4, $5 }' >> `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt
     echo
-    echo " 예시4) awk '{print NR, \$4}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) NR 변수는 각 레코드의 행번호를 출력해준다."
-    awk '{print NR, $4}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo " echo 'Kr Korea :3324 5/11/96 :50354' > `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt "
+    echo " echo 'Jp Japan :5246 15/9/66 :287650' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt "
+    echo " echo 'Us USA :7654 6/20/58 :60000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt "
+    echo " echo 'Cn China :8683 9/40/48 :465000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt "
+    echo 'Kr    Korea   :3324    :5/11/96    :50354' > `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo 'Jp    Japan    :5246    :15/9/66    :287650' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo 'Us    USA    :7654    :6/20/58    :60000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo 'Cn,China,:8683,:9/40/48,:465000' >> `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt
     echo
-    echo " 예시5) awk '{print NF, \$0}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) NF 변수는 공백 또는 탭으로 구분된 분리된 필드의 수(컬럼수)를 출력해준다."
-    awk '{print NF, $0}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : ['/pattern/'] )
     echo
-    echo " 예시6) cat awk_CMD_TestFolder/awk_CMD_TestFile1.txt | awk '\$3 > 7000'"
-    echo " 부가설명) 명령 뒤에 |(파이프)와 함께 조건을 검색해 볼수 있다."
-    cat awk_CMD_TestFolder/awk_CMD_TestFile1.txt | awk '$3 > 7000'
+    echo " Not Option : ['/pattern/']  "
+    echo " matching 'pattern' in file : [ awk '/pattern/' filename ] "
+    echo " awk '/Korea/' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '/Korea/' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
     echo
-    echo " 옵션 -f : awk 명령이 입력되어 있는 파일을 사용하여 다른파일을 처리할 경우 사용"
-    echo " 예시7) awk -f awk_CMD_TestFolder/awk_CMD_Command.txt awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    awk -f awk_CMD_TestFolder/awk_CMD_Command.txt awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    # Example 02 (Not Option : ['{print $n}'] )
     echo
-    echo " 옵션 -F : 필드 구분자는 기본적으로 공백과 탭이 사용되나 -F옵션은 임의의 필드구분자를 지정할 수 있다."
-    echo " 예시8) awk -F: '/Kr/{print \$1, \$2}' awk_CMD_TestFolder/awk_CMD_TestFile2.txt"
-    echo " 부가설명) -F뒤에 필드구분자로 사용할 [ : ] 를 지정하여 필드를 구분한다."
-    awk -F: '/Kr/{print $1, $2}' awk_CMD_TestFolder/awk_CMD_TestFile2.txt
-    echo " 예시9) awk -F'[:\t]' '/Kr/{print \$1, \$2, \$3, \$12}' awk_CMD_TestFolder/awk_CMD_TestFile2.txt"
-    echo " 부가설명) -F뒤에 []를 사용하여 필드구분자를 여러개 지정할 수도 있다."
-    awk -F'[:\t]' '/Kr/{print $1, $2, $3, $12}' awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo " Not Option : ['{print $n}'] "
+    echo " print All Columns in file : [ awk '{print}' filename ] "
+    echo " awk '{print}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '{print}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
     echo
-    echo " 예시10) awk '\$3 > 7000' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) 'pattern'에서는 비교연산자(<,<=,==,!=,>=,>,~,!~), 조건표현식(조건펴현식1 ? 표현식2 : 표현식3), 산술연산자(+,-,*,/,%,^), 논리연산자(&&, ||, !)등을 사용할 수 있다."
-    echo " 부가설명) 비교연산자(~ : 정규식표현과 매칭,!~ : 정규식표현과 비매칭), 산술연산자(^ : 멱수)"
-    awk '$3 > 7000' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo " print All Columns in file : [ awk '{print \$0}' filename ] "
+    echo " awk '{print \$0}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '{print $0}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
     echo
-    echo " 예시11) awk 'BEGIN{FS=\":\"; OFS=\"\\t\"; ORS=\"\\n\\n\"}{print \$1, \$2, \$3}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) BEGIN{}은 awk명령어 바로 뒤에서 선언하며 FS:필드구분자, OFS:출력필드구분자, ORS:출력레코드분리자"
-    awk 'BEGIN{FS=":"; OFS="\t"; ORS="\n\n"}{print $1, $2, $3}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo " print Second Column in file : [ awk '{print \$2}' filename ] "
+    echo " awk '{print \$2}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '{print $2}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
     echo
-    echo " 예시12) awk 'END{print \"Test Number \" NR}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
-    echo " 부가설명) END{}은 awk명령을 모두 출력한뒤 맨 마지막에 출력된다."
-    awk 'END{print "Test Number " NR}' awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    # Example 03 (Not Option : ['/pattern/{print $n, $m}'] )
     echo
-    echo "## 데이터 컨트롤(awk) 종료 ##"
-    echo 
+    echo " Not Option : ['/pattern/{print $n, $m}'] "
+    echo " matching 'pattern' print Third Column and fourth Column in file : [ awk '/pattern/{print \$3, \$4}' filename ] "
+    echo " awk '/Japan/{print \$3, \$4}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '/Japan/{print $3, $4}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 04 (Not Option : ['{print NR, $n }'] )
+    echo
+    echo " Not Option : ['{print NR, $n }'] "
+    echo " Show line Number : [ awk '{print NR, \$4}' filename ] "
+    echo " awk '{print NR, \$4}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '{print NR, $4}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 05 (Not Option : ['{print NF, $n }'] )
+    echo
+    echo " Not Option : ['{print NF, $n }'] "
+    echo " Show Field Count Number : [ awk '{print NF, \$0}' filename ] "
+    echo " awk '{print NF, \$0}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '{print NF, $0}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 06 (Not Option : Comparison of Separater )
+    echo
+    echo " Comparison of Separater "
+    echo " awk '\$3 >=7000' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '$3 >= 7000' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    echo " awk '\$2 ~ /Kor/' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '$2 ~ /Kor/' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    echo " awk '\$2 !~ /Kor/' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk '$2 !~ /Kor/' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 07 (Not Option : BEGIN [FS : Field Separater, OFS : Output Field Separater, ORS : Output Record Separater] )
+    echo
+    echo " Not Option : BEGIN [FS : Field Separater, OFS : Output Field Separater, ORS : Output Record Separater] "
+    echo " awk 'BEGIN{FS=\":\"; OFS=\"\\t\"; ORS=\"\\n\\n\"}{print \$1, \$2, \$3}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
+    awk 'BEGIN{FS=":"; OFS="\t"; ORS="\n\n"}{print $1, $2, $3}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 08 (Option : END [Last Print])
+    echo
+    echo " Option : END [Last Print] "
+    echo " awk 'END{print \$0 \"Test Number \" NR}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt"
+    awk 'END{print $0 "Test Number " NR}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 09 (Option -f : awk Script File Read )
+    echo
+    echo " Option -f : awk Script File Read "
+    echo " awk Script File Read "
+    echo " cat `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt "
+    cat `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt 
+    echo " awk -f `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt "
+    awk -f `pwd`/awk_CMD_TestFolder/awk_CMD_Command.txt `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile1.txt
+    echo
+    # Example 10 (Option -F : Specifying Separater )
+    echo
+    echo " Option -F : Specifying Separater "
+    echo " awk -F: '{print \$1, \$2, \$3}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt "
+    awk -F: '{print $1, $2, $3}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo
+    echo " awk -F'[:,]' '{print \$1, \$2, \$3}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt "
+    awk -F'[:,]' '{print $1, $2, $3}' `pwd`/awk_CMD_TestFolder/awk_CMD_TestFile2.txt
+    echo
+    # Example End
+    echo
+    echo "## awk End ##"
+    echo
 }
 
-DataControlAwk
+awk_DataControlAwk
