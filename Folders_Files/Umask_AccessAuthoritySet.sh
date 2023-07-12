1b01746efdec2a1b9edfbfcd6ff0f8d5e98e1899
DokBak <photoismylife0216@gmail.com>
2023-07-13 00:47:21 +0900

    FIX : Umask_AccessAuthoritySet.sh
    Not Option
    Option -S

--- Folders_Files/Umask_AccessAuthoritySet.sh
+++ Folders_Files/Umask_AccessAuthoritySet.sh
@@ -1,63 +1,88 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 접근 권한 기본 설정
-#-------------------------
-function AccessAuthoritySet(){
-    echo 
-    echo "## 접근 권한 기본 설정 시작 ##"
+#------------------------------------------------------------
+# Cmd Test Shell : [Umask]_[AccessAuthoritySet]
+#------------------------------------------------------------
+function umask_AccessAuthoritySet(){
     echo
-    echo " 명령어  : umask "
-    echo " 사용방법 : umask [옵션] [마스크값]"
-    echo " 기본설명 : 파일 혹은 디렉토리가 생성될때 기본적으로 설정되는 접근모드를 지정하는데 마스크값은 해당 접근권한을 부여하지 않겠다는 의미이다. (관리자의 경우 기본 마스크 값:022)"
+    echo "## umask Start ##"
     echo
-    echo " 사전실행) umask 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir -p umask_CMD_TestFolder1"
-    mkdir -p umask_CMD_TestFolder1
-    cd umask_CMD_TestFolder1
-    echo 'test apple' > umask_CMD_TestFile1.txt
-    echo 
-    echo "ls -l umask_CMD_TestFile1.txt"
-    ls -l umask_CMD_TestFile1.txt
-    echo "현재 내 피시의 일반 파일의 기본 접근모드는 644(-rw-r--r--)"
+    # Basic Information Start
     echo
-    mkdir -p umask_CMD_TestFolder1
-    echo "ls -ld umask_CMD_TestFolder1"
-    ls -ld umask_CMD_TestFolder1
-    echo "현재 내 피시의 폴더의 기본 접근모드는 755(drwxr-xr-x)"
+    echo " Command  : umask"
+    echo " HowToUse : umask [Option] [Argument1] "
+    echo "            [Argument1] : Set access permission "
+    echo "            [Option : -S] : Outputs default access to characters "
     echo
-    echo " 예시1) umask"
-    echo " 부가설명) 현재 마스크 값은 022"
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : Current mask print )
+    echo
+    echo " my system default mask : 022 "
+    echo " umask "
     umask
+    echo " mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/ "
+    mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/
+    echo " echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/umask_CMD_TestFile1.txt "
+    echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/umask_CMD_TestFile1.txt
+    echo " ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/ "
+    ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/
+    echo " ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/umask_CMD_TestFile1.txt "
+    ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder1/umask_CMD_TestFile1.txt
     echo
-    echo " 옵션 -s : 기본 접근 권한을 문자로 출력"
-    echo " 예시2) umask -S"
-    umask -S
+    # Example 02 (Not Option : mask Set exam )
     echo
-    echo "마스크 값 변경 : umask 000"
+    echo " umask 000 "
     umask 000
-    umask
+    echo " mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/ "
+    mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/
+    echo " echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/umask_CMD_TestFile2.txt "
+    echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/umask_CMD_TestFile2.txt
+    echo " ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/ "
+    ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/
+    echo " ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/umask_CMD_TestFile2.txt "
+    ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder2/umask_CMD_TestFile2.txt
+    echo
+    # Example 03 (Not Option : mask Set exam )
+    echo
+    echo " umask 012 "
+    umask 012
+    echo " mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/ "
+    mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/
+    echo " echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/umask_CMD_TestFile3.txt "
+    echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/umask_CMD_TestFile3.txt
+    echo " ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/ "
+    ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/
+    echo " ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/umask_CMD_TestFile3.txt "
+    ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder3/umask_CMD_TestFile3.txt
+    echo
+    # Example 04 (Not Option : mask Set exam )
+    echo
+    echo " umask 567 "
+    umask 567
+    echo " mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/ "
+    mkdir -p `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/
+    echo " echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/umask_CMD_TestFile4.txt "
+    echo 'test apple' > `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/umask_CMD_TestFile4.txt
+    echo " ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/ "
+    ls -ld `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/
+    echo " ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/umask_CMD_TestFile4.txt "
+    ls -l `pwd`/umask_CMD_TestFolder/umask_CMD_TestFolder4/umask_CMD_TestFile4.txt
+    echo
+    # Example 02 (Option -s : Outputs default access to characters )
+    echo
+    echo " Option -s : Outputs default access to characters "
+    echo " umask -S "
     umask -S
     echo
-    echo " 마스크값 변경 후 실행) umask 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir -p umask_CMD_TestFolder2"
-    cd ..
-    mkdir -p umask_CMD_TestFolder2
-    cd umask_CMD_TestFolder2
-    echo 'test apple' > umask_CMD_TestFile2.txt
-    echo 
-    echo "ls -l umask_CMD_TestFile2.txt"
-    ls -l umask_CMD_TestFile2.txt
-    echo "마스크값을 000으로 하였기 때문에 일반 파일의 기본 접근모드는 모든 권한이 있는 666(-rw-rw-rw-)"
+    # Example End
     echo
-    mkdir -p umask_CMD_TestFolder2
-    echo "ls -ld umask_CMD_TestFolder2"
-    ls -ld umask_CMD_TestFolder2
-    echo "마스크값을 000으로 하였기 때문에 폴더의 기본 접근모드는 모든 권한이 있는 777(drwxrwxrwx)"
+    echo "## umask End ##"
     echo
-    cd ..
-    echo "## 접근 권한 기본 설정 종료 ##"
-    echo 
 }
 
-AccessAuthoritySet
+umask_AccessAuthoritySet
