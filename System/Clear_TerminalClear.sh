ded8563837ae98a599a6009bbc2afea34b36ab1c
DokBak <photoismylife0216@gmail.com>
2023-07-14 01:55:41 +0900

    FIX : Clear_TerminalClear.sh
    Not Option

--- System/Clear_TerminalClear.sh
+++ System/Clear_TerminalClear.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 터미널 화면 초기화
-#-------------------------
-function TerminalClear(){
-    echo 
-    echo "## 터미널 화면 초기화 시작 ##"
-    echo " 명령어  : clear"
-    echo " 사용방법 : clear"
-    echo " 기본설명 : 터미널 화면의 내용을 초기화 시킨다. 기존 내용을 엔터로 올리는 것보다 깔끔하게 정리되며, 연속적인 작업후 새로이 작업을 진행할때 사용하면 좋은 명령어"
+#------------------------------------------------------------
+# Cmd Test Shell : [Clear]_[TerminalClear]
+#------------------------------------------------------------
+function clear_TerminalClear(){
     echo
-    echo " 예시1) clear"
+    echo "## clear Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : clear"
+    echo " HowToUse : clear "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " clear "
     clear
     echo
-    echo "## 터미널 화면 초기화 종료 ##"
-    echo 
+    # Example End
+    echo
+    echo "## clear End ##"
+    echo
 }
 
-TerminalClear 
+clear_TerminalClear 
