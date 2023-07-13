c418c818287ee23d2ac43cec48d464c86bbf74dd
DokBak <photoismylife0216@gmail.com>
2023-07-14 01:56:16 +0900

    FIX : Echo_PrintLine.sh
    Not Option
    Option -n

--- System/Echo_PrintLine.sh
+++ System/Echo_PrintLine.sh
@@ -1,28 +1,39 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 기본 출력 처리
-#-------------------------
-function PrintLine(){
-    echo 
-    echo "## 기본 출력 처리 시작 ##"
-    echo " 명령어 : echo"
-    echo " 사용방법 : echo \"[출력할 내용]\""
-    echo " 기본설명 : 문자열을 출력시 echo 명령어를 사용. 빈 줄을 출력할 경우 echo 만 작성, echo \"\"또는 \'\' 추가하여도 좋다."
-    echo " 예시1) 기본 사용법 : echo \"출력 내용\""
-    echo "    옵션 -n : 후행문자를 출력하지 않는다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Echo]_[PrintLine]
+#------------------------------------------------------------
+function echo_PrintLine(){
     echo
-    echo " 예시2) 옵션 -n 사용법 echo -n \"출력 내용\""
-    echo -n "   예시1 출력문"
+    echo "## echo Start ##"
     echo
-    echo " 예시3) echo \"\""
-    echo ""
+    # Basic Information Start
     echo
-    echo " 예시4) echo ''"
-    echo ''
+    echo " Command  : echo"
+    echo " HowToUse : echo [Option] [Argument1] [Argument2] ..."
+    echo "            [Argument1] : tarFile_Path "
+    echo "            [Option : -n] : Keep output in one line without printing open characters "
     echo
-    echo "## 기본 출력 처리 종료 ##"
-    echo 
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " echo ' Test ' "
+    echo ' Test '
+    echo
+    # Example 02 (Option -n : Keep output in one line without printing open characters )
+    echo
+    echo " echo -n ' Test ' "
+    echo -n ' Test '
+    echo
+    # Example End
+    echo
+    echo "## echo End ##"
+    echo
 }
 
-PrintLine 
+echo_PrintLine 
