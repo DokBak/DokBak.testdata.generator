981400cab185a7bf3c996d5524e7e592795fd8fe
DokBak <photoismylife0216@gmail.com>
2023-07-18 00:12:19 +0900

    FIX : Tip_CalculateExpr.sh
    Not Option

--- Script_Tip/Tip_CalculateExpr.sh
+++ Script_Tip/Tip_CalculateExpr.sh
@@ -1,32 +1,46 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 산술 계산(expr)
-#-------------------------
-function ShellScriptCalculateExpr(){
-    echo 
-    echo "## 쉘 스크립트 산술 계산(expr) 시작 ##"
-    echo " 기본설명 : 쉘 스크립트에서 산술 계산을 하는 방법을 확인(expr)"
+#------------------------------------------------------------
+# Cmd Test Shell : [Tip]_[CalculateExpr]
+#------------------------------------------------------------
+function Tip_CalculateExpr(){
     echo
-    echo " 사용법) expr의 산술연산을 이용한다. + : 덧셈, - : 뺼셈, * : 곱셈, / : 나눗셈(몫), % : 나눗셈(나머지)"
-    echo 
+    echo "## CalculateExpr Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " HowToUse : expr Var1 + Var2 "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo " NumVar1=8 "
     NumVar1=8
-    echo "NumVar1 : $NumVar1"
+    echo " NumVar1 : $NumVar1 "
+    echo
+    echo " NumVar2=5 "
     NumVar2=5
-    echo "NumVar2 : $NumVar2"
-    ADDVar=`expr $NumVar1 '+' $NumVar2 `
-    echo "ADDVar : $NumVar1 + $NumVar2 = `expr $NumVar1 '+' $NumVar2 `"
-    SUBVar=`expr $NumVar1 '-' $NumVar2 `
-    echo "SUBVar : $NumVar1 - $NumVar2 = `expr $NumVar1 '-' $NumVar2 `"
-    MULVar=`expr $NumVar1 '*' $NumVar2 `
-    echo "MULVar : $NumVar1 * $NumVar2 = `expr $NumVar1 '*' $NumVar2 `"
-    DIVVar=`expr $NumVar1 '/' $NumVar2 `
-    echo "DIVVar : $NumVar1 / $NumVar2 = `expr $NumVar1 '/' $NumVar2 `"
-    MODVar=`expr $NumVar1 '%' $NumVar2 `
-    echo "MODVar : $NumVar1 % $NumVar2 = `expr $NumVar1 '%' $NumVar2 `"
-    echo 
-    echo "## 쉘 스크립트 산술 계산(expr) 종료 ##"
-    echo 
+    echo " NumVar2 : $NumVar2 "
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
+    ADDVar=` expr $NumVar1 '+' $NumVar2 `
+    echo " ADDVar : $NumVar1 + $NumVar2 = ` expr $NumVar1 '+' $NumVar2 ` "
+    SUBVar=` expr $NumVar1 '-' $NumVar2 `
+    echo " SUBVar : $NumVar1 - $NumVar2 = ` expr $NumVar1 '-' $NumVar2 ` "
+    MULVar=` expr $NumVar1 '*' $NumVar2 `
+    echo " MULVar : $NumVar1 * $NumVar2 = ` expr $NumVar1 '*' $NumVar2 ` "
+    DIVVar=` expr $NumVar1 '/' $NumVar2 `
+    echo " DIVVar : $NumVar1 / $NumVar2 = ` expr $NumVar1 '/' $NumVar2 ` "
+    MODVar=` expr $NumVar1 '%' $NumVar2 `
+    echo " MODVar : $NumVar1 % $NumVar2 = ` expr $NumVar1 '%' $NumVar2 ` "
+    echo
+    # Example End
+    echo
+    echo "## CalculateExpr End ##"
+    echo
 }
 
-ShellScriptCalculateExpr
+Tip_CalculateExpr
