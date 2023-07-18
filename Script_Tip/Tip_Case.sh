95db5f614102299cb0f47aa53f225fd35784192c
DokBak <photoismylife0216@gmail.com>
2023-07-19 00:44:53 +0900

    FIX : Tip_Case.sh
    Not Option

---
+++ Script_Tip/Tip_Case.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+#------------------------------------------------------------
+# Cmd Test Shell : [Tip]_[Case]
+#------------------------------------------------------------
+function Tip_Case(){
+    echo
+    echo "## Case Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : Case"
+    echo " HowToUse : Case [Argument1] in [Var1]) ;; *) esac "
+    echo "            [Var]1 ) : condition "
+    echo "                 * ) : default condition "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
+    echo " insert : kr jp us "
+    echo " read caseVar "
+    read caseVar
+    echo 
+    case "$caseVar" in
+        kr)
+            echo " $caseVar is kr "
+            ;;
+        jp)
+            echo " $caseVar is jp "
+            ;;
+        us)
+            echo " $caseVar is us "
+            ;;
+        *) echo " caseVar ? " # default
+    esac
+    echo
+    # Example 02
+    echo
+    echo " insert : kr jp us "
+    echo " read caseVar "
+    read caseVar
+    case "$caseVar" in kr) echo " $caseVar is kr2 ";; jp) echo " $caseVar is jp2 ";; us) echo " $caseVar is us ";; *) echo " caseVar2 ? "; esac
+    echo
+    # Example End
+    echo
+    echo "## Case End ##"
+    echo
+}
+
+Tip_Case
