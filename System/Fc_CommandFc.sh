71e52ac2233ae4c0225e28af2f98c710d4a23f7d
DokBak <photoismylife0216@gmail.com>
2023-07-21 00:35:40 +0900

    FIX : Fc_CommandFc.sh
    Not Option
    Option -l
    Option -r
    Option -n
    Option -m
    Option -e

--- System/Fc_CommandFc.sh
+++ System/Fc_CommandFc.sh
@@ -1,45 +1,70 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 명령어 히스토리 리스트 처리 (fc)
-#-------------------------
-function CommandFc(){
-    echo 
-    echo "## 명령어 히스토리 리스트 처리 (fc) 시작 ##"
-    echo " 명령어  : fc"
-    echo " 사용방법 : fc"
-    echo " 기본설명 : 이전에 사용하였던 커맨드를 수정하고 재실행한다. (스크립트 상에서는 출력되지 않는다.)"
+#------------------------------------------------------------
+# Cmd Test Shell : [Fc]_[CommandFc]
+#------------------------------------------------------------
+function fc_CommandFc(){
     echo
-    echo " 사전실행) fc 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir fc_CMD_TestFolder/"
-    mkdir -p fc_CMD_TestFolder
-    echo "    echo 'TestFile1 Testing1' > fc_CMD_TestFolder/fc_CMD_TestFile1.txt"
-    echo 'TestFile1 Testing1' > fc_CMD_TestFolder/fc_CMD_TestFile1.txt
-    echo "    echo 'TestFile2 Testing2' > fc_CMD_TestFolder/fc_CMD_TestFile2.txt"
-    echo 'TestFile2 Testing2' > fc_CMD_TestFolder/fc_CMD_TestFile2.txt
+    echo "## fc Start ##"
     echo
-    echo " 예시1) fc -l"
-    echo " 부가설명) -l : 이전 명령어 15줄이 출력된다.(이전에 사용한 명령중 가장 오래된 명령어부터 출력)"
+    # Basic Information Start
+    echo
+    echo " Command  : fc"
+    echo " HowToUse : fc [Option] "
+    echo "            [Option : -l] : 15 lines of previous command output / Output from oldest command "
+    echo "            [Option : -r] : Output from the most recent used command "
+    echo "            [Option : -n] : A history corresponding to an argument value is output "
+    echo "            [Option : -m] : A history corresponding to an argument value is output [n to m] "
+    echo "            [Option : -e] : Use the specified editor "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/fc_CMD_TestFolder/ "
+    mkdir -p `pwd`/fc_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'TestFile1 Testing1' > `pwd`/fc_CMD_TestFolder/fc_CMD_TestFile1.txt "
+    echo 'TestFile1 Testing1' > `pwd`/fc_CMD_TestFolder/fc_CMD_TestFile1.txt
+    echo " echo 'TestFile2 Testing2' > `pwd`/fc_CMD_TestFolder/fc_CMD_TestFile2.txt "
+    echo 'TestFile2 Testing2' > `pwd`/fc_CMD_TestFolder/fc_CMD_TestFile2.txt
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : Edit and enable previous commands )
+    echo
+    echo " fc "
+    fc 
+    echo
+    # Example 02 (Option -l : 15 lines of previous command output / Output from oldest command )
+    echo
+    echo " fc -l "
     fc -l
     echo
-    echo " 예시2) fc -l -r"
-    echo " 부가설명) -r : 역순으로 출력된다.(가장 최근 사용한 명령어부터 출력)"
+    # Example 03 (Option -r : Output from the most recent used command )
+    echo
+    echo " fc -l -r "
     fc -l -r
     echo
-    echo " 예시3) fc -l -r -n"
-    echo " 부가설명) -n : 숫자로 넣은 아규먼트 값만큼의 이력이 출력된다."
+    # Example 04 (Option -n : A history corresponding to an argument value is output )
+    echo
+    echo " fc -l -r -3 "
     fc -l -r -3
     echo
-    echo " 예시4) fc -l -r -n -m"
-    echo " 부가설명) -n -m : n번째부터 m번째까지 명령어 리스트를 출력한다."
+    # Example 05 (Option -n -m : A history corresponding to an argument value is output [n to m] )
+    echo
+    echo " fc -l -r -3 -5 "
     fc -l -r -3 -5
     echo
-    echo " 예시5) fc -e editor"
-    echo " 부가설명) -e editor : 지정한 에디터로 이전에 사용한 명령어를 수정한다."
+    # Example 06 (Option -e : Use the specified editor )
+    echo
+    echo " fc -e vim "
     fc -e vim
     echo
-    echo "## 명령어 히스토리 리스트 처리 (fc) 종료 ##"
-    echo 
+    # Example End
+    echo
+    echo "## fc End ##"
+    echo
 }
 
-CommandFc
+fc_CommandFc
