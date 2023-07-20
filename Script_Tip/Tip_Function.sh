56d55fbdf09bfc8b986ec55d8304a0e44c8103dd
DokBak <photoismylife0216@gmail.com>
2023-07-21 00:52:58 +0900

    FIX : Tip_Function.sh
    Not Option

--- Script_Tip/Tip_Function.sh
+++ Script_Tip/Tip_Function.sh
@@ -1,44 +1,59 @@
 #!/bin/sh
 
-
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 함수
-#-------------------------
-function ShellScriptFunction(){
-    echo 
-    echo "## 쉘 스크립트 함수 시작 ##"
-    echo " 기본설명 : 쉘 스크립트에서 함수를 선언하고 사용하는 방법을 확인"
+#------------------------------------------------------------
+# Cmd Test Shell : [Tip]_[Function]
+#------------------------------------------------------------
+function Tip_Function(){
     echo
-    echo " 부가설명) 함수뒤에 작성되는 값들은 모두 파라미터로 적용된다."
+    echo "## Function Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : Function"
+    echo " HowToUse : Function [Argument1] [Argument2] ..."
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
     TestFunc1 TestParam1
     echo
-    echo " 부가설명) 함수1 함수2 파라미터를 작성하여도 함수1의 파라미터가 2개인것으로 인식되어 실행된다."
-    TestFunc1 TestFunc2 TestParam2
-    echo 
-    echo " 부가설명) 함수 선언시 function을 생략하여 작성하는 방법으로 함수를 선언하여도 된다."
-    TestFunc3
-    echo 
-    echo " 부가설명) 함수명뒤의 () 소괄호를 작성하지 않아도 함수로 인식하여 실행된다."
-    TestFunc4
+    # Example 02
     echo
-    echo "## 쉘 스크립트 함수 종료 ##"
-    echo 
+    TestFunc1 TestFunc2 TestParam2
+    echo
+    # Example 03
+    echo
+    TestFunc3
+    echo
+    # Example 04
+    echo
+    TestFunc3
+    echo
+    # Example End
+    echo
+    echo "## Function End ##"
+    echo
 }
 
 function TestFunc1(){
-    echo "TestFunc1 함수 시작"
+    echo "TestFunc1 Start "
     echo "TestFunc1, Param1 : $1"
-    echo "TestFunc1 함수 종료"
+    echo "TestFunc1 End "
 }
 
 function TestFunc2(){
-    echo "TestFunc2 함수 시작"
+    echo "TestFunc2 Start "
     echo "TestFunc2, Param1 : $1"
-    echo "TestFunc2 함수 종료"
+    echo "TestFunc2 End "
 }
 
 TestFunc3() { echo "TestFunc3 print1"; echo "TestFunc3 print2"; }
 
 function TestFunc4 { echo "TestFunc4 print1"; echo "TestFunc4 print2"; }
 
-ShellScriptFunction
+Tip_Function
