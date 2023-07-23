13d5216ae91dddbac6166bfda7fac452d1444427
DokBak <photoismylife0216@gmail.com>
2023-07-23 18:46:56 +0900

    FIX : IFS_DataSeparator.sh
    Not Option

--- Script_Tip/IFS_DataSeparator.sh
+++ Script_Tip/IFS_DataSeparator.sh
@@ -1,36 +1,53 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 내부 필드 분리자
-#-------------------------
-function ShellScriptIFS(){
-    echo 
-    echo "## 쉘 스크립트 내부 필드 분리자 시작 ##"
+#------------------------------------------------------------
+# Cmd Test Shell : [IFS]_[DataSeparator]
+#------------------------------------------------------------
+function IFS_DataSeparator(){
     echo
-    echo " 기본설명 : read, set, for 등 단어목록을 분석하는 명령을 사용시 내부필드분리자값을 지정하여 사용할 수도 있다. 기본적으로는 공백, 탭, newline등의 문자를 평가한다."
+    echo "## IFS Start ##"
     echo
-    echo " :로 구분되어진 nation변수를 선언 -> nation=Korea:Japan:China:USA"
+    # Basic Information Start
     echo
-    nation=Korea:Japan:China:USA
-    echo " IFS 기본값으로 결과 값을 확인(예시 for문)"
+    echo " Command  : IFS"
+    echo " HowToUse : IFS=[Argument1] "
+    echo "            [Argument1] : DataSeparator "
+    echo
+    # Basic Information End
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
+    echo ' nation=Korea Japan:China:USA '
+    nation="Korea Japan:China:USA"
+    NumValue=0
+    echo " IFS Defualt : Space "
     for country in $nation
     do  
         NumValue=$(( NumValue + 1 ))
-        echo " : 로 구분되진 $NumValue 번째 데이터 $country"
+        echo " Defualt : Space -> No. $NumValue : $country"
     done
     echo
-    echo " 기존 IFS값을 백업후 :로 재지정후 결과 값을 확인(예시 for문)"
+    # Example 02
+    echo
+    echo ' nation=Korea Japan:China:USA '
+    nation="Korea Japan:China:USA"
+    NumValue=0
     oldifs="$IFS"
     IFS=":"
-    NumValue=0
+    echo " IFS : " : " "
     for country in $nation
     do  
         NumValue=$(( NumValue + 1 ))
-        echo " : 로 구분되진 $NumValue 번째 데이터 $country"
+        echo " IFS : Space -> No. $NumValue : $country"
     done
     echo
-    echo "## 쉘 스크립트 내부 필드 분리자 종료 ##"
-    echo 
+    # Example End
+    echo
+    echo "## IFS End ##"
+    echo
 }
 
-ShellScriptIFS
+IFS_DataSeparator
