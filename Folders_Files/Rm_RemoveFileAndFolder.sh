c91922ea32338cd2710d5a1597d7a80129fdd69b
DokBak <photoismylife0216@gmail.com>
2023-07-26 21:41:11 +0900

    FIX : Rm_RemoveFileAndFolder.sh
    Not Option
    Option -i
    Option -r
    Option -f
    option -v

--- Folders_Files/Rm_RemoveFileAndFolder.sh
+++ Folders_Files/Rm_RemoveFileAndFolder.sh
@@ -1,103 +1,94 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 파일(폴더) 삭제 처리
-#-------------------------
-function RemoveFileAndFolder(){
-    echo 
-    echo "## 파일(폴더) 삭제 처리 시작 ##"
-    echo " 명령어  : rm"
-    echo " 사용방법 : rm [옵션] [디렉토리명 or 파일명]"
-    echo " 기본설명 : 지정한 파일을 삭제 / 폴더만 삭제하는 경우는 rmdir을 사용하며, 폴더 및 파일을 모두 사용시에는 rm명령어를 애용한다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Rm]_[RemoveFileAndFolder]
+#------------------------------------------------------------
+function rm_RemoveFileAndFolder(){
     echo
-    echo " 사전실행) rm 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/"
-    mkdir -p Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/
-    echo "    echo 'TestFile1' > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt"
-    echo "TestFile1" > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt
-    echo "    echo 'TestFile2' > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt"
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "    echo 'TestFile2' > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt"
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "    echo 'TestFile3' > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt"
-    echo "TestFile3" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt
-    echo "    echo 'TestFile4' > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt"
-    echo "TestFile4" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    echo "    echo 'TestFile5' > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt"
-    echo "TestFile5" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
+    echo "## rm Start ##"
     echo
-    echo " 예시1) rm Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt"
-    rm Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    ls -lR
+    # Basic Information Start
     echo
-    echo " 옵션 -i : 파일, 디렉토리를 삭제시 항목 별로 질문하는 옵션(-f보다 우선 순위가 높다 / 파일 및 폴더 하나 하나 질문한다.)"
-    echo " 예시2) rm -i Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt"
-    mkdir -p Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/
-    echo "TestFile1" > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile3" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt
-    echo "TestFile4" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    echo "TestFile5" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
-    rm -i Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
+    echo " Command  : rm"
+    echo " HowToUse : rm [Option] [Argument1] "
+    echo "            [Argument1] : tarFile_Path "
+    echo "            [Option : -i] : Ask questions when deleting files or folders one by one "
+    echo "            [Option : -r] : delete File of SubDirectories "
+    echo "            [Option : -f] : force delete File "
+    echo "            [Option : -v] : Output detailed information "
     echo
-    echo " 옵션 -r : 디렉토리 지정시 하위 경로까지 삭제하는 옵션"
-    echo " 예시3) rm -r Rm_CMD_TestFolder/Rm_CMD_TestFolder2/"
-    mkdir -p Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/
-    echo "TestFile1" > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile3" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt
-    echo "TestFile4" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    echo "TestFile5" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
-    rm -r Rm_CMD_TestFolder/Rm_CMD_TestFolder2/
-    ls -lR
+    # Basic Information End
+    # Preparation Start
     echo
-    echo " 옵션 -f : 파일 디렉토리 삭제시 강제로 삭제하는 옵션 (단독으로 사용되지는 않는다.)"
-    echo " 예시4) rm -rf Rm_CMD_TestFolder/Rm_CMD_TestFolder2/"
-    mkdir -p Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/
-    echo "TestFile1" > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile3" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt
-    echo "TestFile4" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    echo "TestFile5" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
-    rm -rf Rm_CMD_TestFolder/Rm_CMD_TestFolder2/
-    ls -lR
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/rm_CMD_TestFolder3/ "
+    mkdir -p `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/rm_CMD_TestFolder3/
+    # Preparation : Create File
+    echo " echo 'TestFile1' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile1.txt "
+    echo 'TestFile1' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile1.txt
+    echo " echo 'TestFile2' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile2.txt "
+    echo 'TestFile2' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile2.txt
+    echo " echo 'TestFile3' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile3.txt "
+    echo 'TestFile3' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile3.txt
+    echo " echo 'TestFile4' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/rm_CMD_TestFile4.txt "
+    echo 'TestFile4' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/rm_CMD_TestFile4.txt
+    echo " echo 'TestFile5' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/rm_CMD_TestFile5.txt "
+    echo 'TestFile5' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/rm_CMD_TestFile5.txt
+    echo " echo 'TestFile6' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder4/rm_CMD_TestFile6.txt "
+    echo 'TestFile6' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder4/rm_CMD_TestFile6.txt
+    echo " echo 'TestFile7' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder4/rm_CMD_TestFile7.txt "
+    echo 'TestFile7' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder4/rm_CMD_TestFile7.txt
+    echo " echo 'TestFile8' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder4/rm_CMD_TestFile8.txt "
+    echo 'TestFile8' > `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder4/rm_CMD_TestFile8.txt
     echo
-    echo " 옵션 -I : 파일 3개 이상일 경우 한번만 묻는다."
-    echo " 예시5) rm -rfI Rm_CMD_TestFolder/"
-    mkdir -p Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/
-    echo "TestFile1" > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile3" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt
-    echo "TestFile4" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    echo "TestFile5" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
-    rm -rfI Rm_CMD_TestFolder/
-    ls -lR
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : delete File ) 
     echo
-    echo " 옵션 -v : 파일을 삭제될 파일, 폴더 순으로 출력되며 최종적으로 남는 파일도 표시해준다."
-    echo " 예시5) rm -rfv Rm_CMD_TestFolder/"
-    mkdir -p Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/
-    echo "TestFile1" > Rm_CMD_TestFolder/Rm_CMD_TestFile1.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile2" > Rm_CMD_TestFolder/Rm_CMD_TestFile2.txt
-    echo "TestFile3" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFile3.txt
-    echo "TestFile4" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile4.txt
-    echo "TestFile5" > Rm_CMD_TestFolder/Rm_CMD_TestFolder2/Rm_CMD_TestFolder3/Rm_CMD_TestFile5.txt
-    ls -lR
-    rm -rfv Rm_CMD_TestFolder/
-    ls -lR
+    echo " Not Option : delete File "
+    echo " rm `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile1.txt "
+    rm `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile1.txt
+    echo " ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile1.txt "
+    ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile1.txt
     echo
-    echo "## 파일(폴더) 삭제 처리 종료 ##"
-    echo 
+    # Example 02 (Option -i : Ask questions when deleting files or folders one by one ) 
+    echo
+    echo " Option -i : Ask questions when deleting files or folders one by one "
+    echo " rm -i `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile2.txt "
+    rm -i `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile2.txt
+    echo " ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile2.txt "
+    ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile2.txt
+    echo
+    # Example 03 (Option -r : delete File of SubDirectories ) 
+    echo
+    echo " Option -r : delete File of SubDirectories "
+    echo " rm -r `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/ "
+    rm -r `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/
+    echo " ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/ "
+    ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder2/
+    echo
+    # Example 04 (Option -f : force delete File ) 
+    echo
+    echo " Option -f : force delete File "
+    echo " rm -rf `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/ "
+    rm -rf `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/
+    echo " ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/rm_CMD_TestFile5.txt "
+    ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/rm_CMD_TestFile5.txt
+    echo " ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/ "
+    ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFolder3/rm_CMD_TestFolder4/
+    echo
+    # Example 05 (Option -v : Output detailed information ) 
+    echo
+    echo " Option -v : Output detailed information "
+    echo " rm -v `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile3.txt "
+    rm -v `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile3.txt
+    echo " ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile3.txt "
+    ls -l `pwd`/rm_CMD_TestFolder/rm_CMD_TestFile3.txt
+    echo
+    # Example End
+    echo
+    echo "## rm End ##"
+    echo
 }
 
-RemoveFileAndFolder
+rm_RemoveFileAndFolder
