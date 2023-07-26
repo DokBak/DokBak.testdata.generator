3f7d738a91be54dfce2a8afffedc17f48ffadc40
DokBak <photoismylife0216@gmail.com>
2023-07-27 00:57:29 +0900

    FIX : Pwd_ShowPwd.sh
    Not Option
    Option -L

--- Folders_Files/Pwd_ShowPwd.sh
+++ Folders_Files/Pwd_ShowPwd.sh
@@ -1,27 +1,42 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 작업 디렉토리 출력 처리
-#-------------------------
-function ShowPwd(){
-    echo 
-    echo "## 작업 디렉토리 출력 시작 ##"
-    echo " 명령어  : pwd"
-    echo " 사용방법 : pwd [파라미터없음]"
-    echo " 기본설명 : 현재 작업 디렉토리를 출력"
-    echo " 예시1) pwd"
+#------------------------------------------------------------
+# Cmd Test Shell : [Pwd]_[ShowPwd]
+#------------------------------------------------------------
+function pwd_ShowPwd(){
+    echo
+    echo "## pwd Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : pwd"
+    echo " HowToUse : pwd [Option] "
+    echo "            [Option : -L : Current Working Directory [Default Option] "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/pwd_CMD_TestFolder/ "
+    mkdir -p `pwd`/pwd_CMD_TestFolder/
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " pwd "
     pwd
     echo
-    echo " 옵션 -L : pwd만 입력하였을때 적용되는 옵션(디폴트값)"
-    echo " 예시2) pwd -L"
+    # Example 02 (Option -L : Current Working Directory [Default Option] )
+    echo
+    echo " Option -L : Current Working Directory [Default Option] "
+    echo " pwd -L "
     pwd -L
     echo
-    echo " 옵션 -P : 심볼릭 링크 경로의 경우. 심볼릭 링크 경로의 절대 경로가 출력된다."
-    echo " 예시3) pwd -P"
-    pwd -P
+    # Example End
     echo
-    echo "## 작업 디렉토리 출력 종료 ##"
-    echo 
+    echo "## pwd End ##"
+    echo
 }
 
-ShowPwd
+pwd_ShowPwd
