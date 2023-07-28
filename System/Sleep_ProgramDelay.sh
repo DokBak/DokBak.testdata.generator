b3abe41a11717c2a4e07c3ba0125434505411229
DokBak <photoismylife0216@gmail.com>
2023-07-28 21:23:20 +0900

    FIX : Sleep_ProgramDelay.sh
    Not Option

--- System/Sleep_ProgramDelay.sh
+++ System/Sleep_ProgramDelay.sh
@@ -1,35 +1,33 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 작업 간 중간 딜레이 부여
-#-------------------------
-function ProgramDelay(){
-    echo 
-    echo "## 작업 간 중간 딜레이 부여 시작 ##"
-    echo " 명령어  : sleep"
-    echo " 사용방법 : sleep [delayTime(s)]"
-    echo " 기본설명 : 프로그램이 순차적으로 작성되어 있는 경우에는 문제가 없는 경우가 많지만, 병렬처리로 작성되어 있는 경우에는 강제적으로 순차적 진행하도록 딜레이를 부여할 수 있다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Sleep]_[ProgramDelay]
+#------------------------------------------------------------
+function sleep_ProgramDelay(){
     echo
-    echo " 예시1) echo 'Test print step1'"
-    echo "       sleep 1"
-    echo "       echo 'Test print step2'"
-    echo "       sleep 1"
-    echo "       echo 'Test print step3'"
-    echo "       sleep 1"
-    echo "       echo 'Test print step4'"
-    echo "       sleep 3"
+    echo "## sleep Start ##"
     echo
-    echo 'Test print step1'
-    sleep 1
-    echo 'Test print step2'
-    sleep 1
-    echo 'Test print step3'
-    sleep 1
-    echo 'Test print step4'
-    sleep 3
+    # Basic Information Start
     echo
-    echo "## 작업 간 중간 딜레이 부여 종료 ##"
-    echo 
+    echo " Command  : sleep"
+    echo " HowToUse : sleep [Argument1] "
+    echo "            [Argument1] : delayTime(s) "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " sleep 2"
+    sleep 2
+    echo
+    # Example End
+    echo
+    echo "## sleep End ##"
+    echo
 }
 
-ProgramDelay
+sleep_ProgramDelay
