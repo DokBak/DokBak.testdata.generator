6a0da3c8ff5cef2bd892d4e8dfb604fce96104fb
DokBak <photoismylife0216@gmail.com>
2023-07-29 00:36:52 +0900

    FIX : While_WhileLoop.sh
    Not Option

--- Script_Tip/While_WhileLoop.sh
+++ Script_Tip/While_WhileLoop.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 반복문(while)
-#-------------------------
-function ShellScriptLoopWhile(){
-    echo 
-    echo "## 쉘 스크립트 반복문(while) 시작 ##"
-    echo " 기본설명 : 쉘 스크립트에서 반복문을 사용하는 방법을 확인"
+#------------------------------------------------------------
+# Cmd Test Shell : [While]_[WhileLoop]
+#------------------------------------------------------------
+function while_WhileLoop(){
     echo
-    echo " 사용법) while [ 종료 조건 ]; do [ 실행문 ]; done "
-    echo " 부가설명) 종료 조건이 거짓이 되기 전까지 do 와 done 사이의 처리가 무한 반복한다.(참인 경우 실행)"
-    echo 
+    echo "## while Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : while"
+    echo " HowToUse : While [ loop_condition ]; do [ content ]; done "
+    echo "           [ loop_condition ] true is run "
+    echo
+    # Basic Information End
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
     number=0
     while [ $number -lt 10 ] 
     do
@@ -18,11 +27,14 @@ function ShellScriptLoopWhile(){
     number=`expr $number + 1`
     done
     echo 
-    echo " 한 줄 사용법 : while, do, done 이외에는 ;를 추가하여 한줄이 종료되었다는 것을 알린다."
+    # Example 02
+    echo
     while [ $number -lt 20 ]; do echo "$number"; number=`expr $number + 1`; done
-    echo 
-    echo "## 쉘 스크립트 반복문(while) 종료 ##"
-    echo 
+    echo
+    # Example End
+    echo
+    echo "## while End ##"
+    echo
 }
 
-ShellScriptLoopWhile
+while_WhileLoop
