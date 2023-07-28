bb4a19cdb67b2230a7aefd8b8d78b148d87310ec
DokBak <photoismylife0216@gmail.com>
2023-07-28 21:26:19 +0900

    FIX : History_CommandHistory.sh
    Not Option

--- System/History_CommandHistory.sh
+++ System/History_CommandHistory.sh
@@ -1,34 +1,40 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 명령어 히스토리 확인 (history)
-#-------------------------
-function CommandHistory(){
-    echo 
-    echo "## 명령어 히스토리 확인 (history) 시작 ##"
-    echo " 명령어  : history"
-    echo " 사용방법 : history"
-    echo " 기본설명 : 이전에 사용하였던 커맨드를 출력한다. (스크립트 상에서는 출력되지 않는다.)"
+#------------------------------------------------------------
+# Cmd Test Shell : [History]_[CommandHistory]
+#------------------------------------------------------------
+function history_CommandHistory(){
     echo
-    echo " 사전실행) history 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir history_CMD_TestFolder/"
-    mkdir -p history_CMD_TestFolder
-    echo "    echo 'TestFile1 Testing1' > history_CMD_TestFolder/history_CMD_TestFile1.txt"
-    echo 'TestFile1 Testing1' > history_CMD_TestFolder/history_CMD_TestFile1.txt
-    echo "    echo 'TestFile2 Testing2' > history_CMD_TestFolder/history_CMD_TestFile2.txt"
-    echo 'TestFile2 Testing2' > history_CMD_TestFolder/history_CMD_TestFile2.txt
+    echo "## history Start ##"
     echo
-    echo " 예시1) history"
-    echo " 부가설명) 이전 명령어 15줄이 출력된다."
+    # Basic Information Start
+    echo
+    echo " Command  : history"
+    echo " HowToUse : history "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/history_CMD_TestFolder/ "
+    mkdir -p `pwd`/history_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'TestFile1 Testing1' > `pwd`/history_CMD_TestFolder/history_CMD_TestFile1.txt "
+    echo 'TestFile1 Testing1' > `pwd`/history_CMD_TestFolder/history_CMD_TestFile1.txt
+    echo " echo 'TestFile2 Testing2' > `pwd`/history_CMD_TestFolder/history_CMD_TestFile2.txt "
+    echo 'TestFile2 Testing2' > `pwd`/history_CMD_TestFolder/history_CMD_TestFile2.txt
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " history "
     history
     echo
-    echo " 예시2) history 1"
-    echo " 부가설명) 아규먼트로 숫자를 넣게되면 첫번째 명령부터 사용했던 모든 이력이 출력된다."
-    echo " 부가설명) 또는 숫자로 넣은 아규먼트 값만큼의 이력이 출력된다."
-    history 1
+    # Example End
     echo
-    echo "## 명령어 히스토리 확인 (history) 종료 ##"
-    echo 
+    echo "## history End ##"
+    echo
 }
 
-CommandHistory
+history_CommandHistory
