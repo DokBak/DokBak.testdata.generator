75addf4c58960b3fcf16b15463fb4cfdedae77c1
DokBak <photoismylife0216@gmail.com>
2023-07-30 11:40:36 +0900

    FIX : Tip_Semicolon_CommandEnd.sh
    Not Option

--- Script_Tip/Tip_Semicolon_CommandEnd.sh
+++ Script_Tip/Tip_Semicolon_CommandEnd.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 다중명령
-#-------------------------
-function ShellScriptMultiCommand(){
-    echo 
-    echo "## 쉘 스크립트 다중명령 시작 ##"
-    echo " 기본설명 : 쉘 스크립트에서 한 줄에 여러 명령을 수행하는 방법"
+#------------------------------------------------------------
+# Cmd Test Shell : [Tip]_[Semicolon_CommandEnd]
+#------------------------------------------------------------
+function Tip_semicolon_CommandEnd(){
     echo
-    echo " 사용법) 한 줄에 여러 명령을 실행하는 방법. 하나의 명령 뒤에 ;로 명령어 종료를 알려주면된다."
-    echo 
-    echo " 예시1) cal; pwd; date;"
+    echo "## semicolon Start ##"
     echo
+    # Basic Information Start
+    echo
+    echo " Command  : semicolon"
+    echo " HowToUse : [Command] ; "
+    echo
+    # Basic Information End
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
+    echo " cal; pwd; date; "
     cal; pwd; date;
-    echo 
-    echo "## 쉘 스크립트 다중명령 종료 ##"
-    echo 
+    echo
+    # Example End
+    echo
+    echo "## semicolon End ##"
+    echo
 }
 
-ShellScriptMultiCommand
+Tip_semicolon_CommandEnd
