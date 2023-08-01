266e7f125a1d1db17272c152d8037646cd1eb435
DokBak <photoismylife0216@gmail.com>
2023-08-01 19:44:13 +0900

    FIX : Tip_Mark_CommandExclamationMark.sh
    Not Option

--- Script_Tip/Tip_Mark_CommandExclamationMark.sh
+++ Script_Tip/Tip_Mark_CommandExclamationMark.sh
@@ -1,54 +1,48 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 명령어 히스토리 목록 처리 (!)
-#-------------------------
-function CommandExclamationMark(){
+#------------------------------------------------------------
+# Cmd Test Shell : [Tip]_[Mark_CommandExclamationMark]
+#------------------------------------------------------------
+function Tip_Mark_CommandExclamationMark(){
+    echo
+    echo "## mark Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : !"
+    echo " HowToUse : ! "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : mkdir
+    echo " Preparation "
+    # Preparation : Create File
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " !!   : Execute previous command "
+    echo
+    echo " !3   : Execute the 3th command "
+    echo
+    echo " !-3  : Execute the 3th command "
+    echo
+    echo " !p   : Recently executed commands that begin with p "
     echo 
-    echo "## 명령어 히스토리 목록 처리 (!) 시작 ##"
-    echo " 명령어  : !"
-    echo " 사용방법 : !"
-    echo " 기본설명 : !는 이벤트 지시자로 !뒤에 오는 문자로 최근 실행하였던 명령을 실행한다. (스크립트 상에서는 사용할수 없다.)"
+    echo " !?at : Execute the most recently used command containing at "
     echo
-    echo " 예시1) !!"
-    echo " 부가설명) ! : 바로 직전의 명령어를 실행"
+    echo " !:2  : Get the 2th argument from the previously used command "
     echo
-    echo " 예시2) pwd"
-    echo " 예시2) !n"
-    echo " 부가설명) n : n번째로 실행한 명령어를 실행(실패한 명령어여도 실행)"
+    echo " !^   : Get the 1th argument from the previously used command "
     echo
-    echo " 예시3) pwd"
-    echo " 예시3) !-n"
-    echo " 부가설명) -n : 현재 명령행에서 n을 뺀 행에 실행한 명령어를 실행(실패한 명령어여도 실행)"
+    echo " !*   : Get the all argument from the previously used command "
     echo
-    echo " 예시4) pwd"
-    echo " 예시4) date"
-    echo " 예시4) !p"
-    echo " 부가설명) p : p로 시작하는 가장 최근에 사용한 명령어를 실행(실패한 명령어여도 실행)"
+    # Example End
     echo
-    echo " 예시5) date"
-    echo " 예시5) pwd"
-    echo " 예시5) !?at"
-    echo " 부가설명) ?at : at를 포함하는 가장 최근에 사용한 명령어를 실행(실패한 명령어여도 실행)"
+    echo "## mark End ##"
     echo
-    echo " 예시6) echo a s d f"
-    echo " 예시6) echo !:2"
-    echo " 부가설명) !:n : 이전에 사용하였던 [echo a s d f] 명령에서 n번째 아규먼트를 가져온다."
-    echo
-    echo " 예시7) echo a s d f"
-    echo " 예시7) echo !^"
-    echo " 부가설명) !^ : 이전에 사용하였던 [echo a s d f] 명령에서 첫번째 아규먼트를 가져온다."
-    echo
-    echo " 예시8) echo a s d f"
-    echo " 예시8) echo !^"
-    echo " 부가설명) !^ : 이전에 사용하였던 [echo a s d f] 명령에서 마지막 아규먼트를 가져온다."
-    echo
-    echo " 예시9) echo a s d f"
-    echo " 예시9) echo !*"
-    echo " 부가설명) !* : 이전에 사용하였던 [echo a s d f] 명령에서 사용한 모든 아규먼트를 가져온다."
-    echo
-    echo "## 명령어 히스토리 목록 처리 (!) 종료 ##"
-    echo 
 }
 
-CommandExclamationMark
+Tip_Mark_CommandExclamationMark
