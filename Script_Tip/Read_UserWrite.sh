fe2f31f7f7150d9dede8ac8760966fdcfeac1ed3
DokBak <photoismylife0216@gmail.com>
2023-08-02 02:30:32 +0900

    FIX : Read_UserWrite.sh
    Not Option
    Option -p
    Option -n
    Option -t
    Option -s

--- Script_Tip/Read_UserWrite.sh
+++ Script_Tip/Read_UserWrite.sh
@@ -1,24 +1,62 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 사용자 입력
-#-------------------------
-function ShellScriptUserWrite(){
-    echo 
-    echo "## 쉘 스크립트 사용자 입력 시작 ##"
-    echo " 기본설명 : 쉘 스크립트에서 유저로부터 입력값을 받아 사용할때의 방법을 확인"
+#------------------------------------------------------------
+# Cmd Test Shell : [Read]_[UserWrite]
+#------------------------------------------------------------
+function read_UserWrite(){
     echo
-    echo "name : "
+    echo "## read Start ##"
+    echo
+    # Basic Information Start
+    echo
+    echo " Command  : read"
+    echo " HowToUse : read [Option] [Argument1] "
+    echo "            [Argument1] : Variable "
+    echo "            [Option : -p] : Set the value to output before receiving input from the user "
+    echo "            [Option : -n] : Set the number of characters to be entered "
+    echo "            [Option : -t] : Set the time to wait for input "
+    echo "            [Option : -s] : Enables you to receive input in a hidden form [Like the password] "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : Receiving Inputs from Users )
+    echo
+    echo " name : "
     read name
-    echo "sex : "
-    read sex
-    echo "tel : "
-    read tel
-    echo 
-    echo "입력 값 출력 name : $name, sex : $sex, tel : $tel"
-    echo 
-    echo "## 쉘 스크립트 사용자 입력 종료 ##"
-    echo 
+    echo " name : $name "
+    echo
+    # Example 02 (Option -p : Set the value to output before receiving input from the user )
+    echo
+    echo " tel : "
+    read -p " input your number : " tel
+    echo " tel : $tel "
+    echo
+    # Example 03 (Option -n : Set the number of characters to be entered )
+    echo
+    echo " nickName : "
+    read -n 4 nickName
+    echo " nickName : $nickName "
+    echo
+    # Example 04 (Option -t : Set the time to wait for input )
+    echo
+    echo " waittime : "
+    read -t 3 waittime
+    echo " waittime : $waittime "
+    echo
+    # Example 05 (Option -s : Enables you to receive input in a hidden form [Like the password] )
+    echo
+    echo " hideString : "
+    read -s hideString
+    echo " hideString : $hideString "
+    echo
+    # Example End
+    echo
+    echo "## read End ##"
+    echo
 }
 
-ShellScriptUserWrite
+read_UserWrite
