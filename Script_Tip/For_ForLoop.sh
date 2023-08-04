bf0622ad631d3e140c104e163bd443e83820b5e4
DokBak <photoismylife0216@gmail.com>
2023-08-04 21:48:46 +0900

    FIX : For_ForLoop.sh
    Not Option

--- Script_Tip/For_ForLoop.sh
+++ Script_Tip/For_ForLoop.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 쉘 스크립트 반복문(for)
-#-------------------------
-function ShellScriptLoopFor(){
-    echo 
-    echo "## 쉘 스크립트 반복문(for) 시작 ##"
-    echo " 기본설명 : 쉘 스크립트에서 반복문을 사용하는 방법을 확인"
+#------------------------------------------------------------
+# Cmd Test Shell : [For]_[ForLoop]
+#------------------------------------------------------------
+function for_ForLoop(){
     echo
-    echo " 사용법) for [ 루프 인덱스 ]; in [ 인덱스리스트 ]; do [ 실행문 ] done "
-    echo " 부가설명) 종료 조건이 달성되기 전까지 do 와 done 사이의 처리가 무한 반복한다."
+    echo "## for Start ##"
     echo
-    echo " 예1)"
+    # Basic Information Start
+    echo
+    echo " Command  : for"
+    echo " HowToUse : for [ loop_index ] in [ index_list ]; do [ content ] done "
+    echo
+    # Basic Information End
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01
+    echo
     for indexNum in {0..3}
     do
-    echo $indexNum
+        echo $indexNum
     done 
     echo
-    echo " 예2)"
+    # Example 02
+    echo
     for indexString in Kor Jap
     do
-    echo $indexString
+        echo $indexString
     done
     echo
-    echo " 한 줄 사용법 : for, do, done 이외에는 ;를 추가하여 한줄이 종료되었다는 것을 알린다."
+    # Example 03
+    echo
     for indexNum in {4..9}; do echo $indexNum; done 
     echo
-    echo "## 쉘 스크립트 반복문(for) 종료 ##"
-    echo 
+    # Example End
+    echo
+    echo "## for End ##"
+    echo
 }
 
-ShellScriptLoopFor
+for_ForLoop
