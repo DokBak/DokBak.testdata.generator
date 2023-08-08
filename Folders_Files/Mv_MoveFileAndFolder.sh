6a1c62f39e51b742d447d444827971eb31f55359
DokBak <photoismylife0216@gmail.com>
2023-08-09 01:00:44 +0900

    FIX : Mv_MoveFileAndFolder.sh
    Not Option
    Option -f
    Option -i
    Option -n
    Option -v

--- Folders_Files/Mv_MoveFileAndFolder.sh
+++ Folders_Files/Mv_MoveFileAndFolder.sh
@@ -1,67 +1,100 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 파일(디렉토리) 이동 처리
-#-------------------------
-function MoveFileAndFolder(){
-    echo 
-    echo "## 파일(디렉토리) 이동 처리 시작 ##"
-    echo " 명령어  : mv"
-    echo " 사용방법 : mv [이동할 파일/디렉토리] [목적지 디렉토리]"
-    echo " 기본설명 : 파일 "
+#------------------------------------------------------------
+# Cmd Test Shell : [Mv]_[MoveFileAndFolder]
+#------------------------------------------------------------
+function mv_MoveFileAndFolder(){
     echo
-    echo " 사전실행) mv 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3/"
-    mkdir -p mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3/
-    echo "    echo 'TestFile1' > mv_CMD_TestFolder/mv_CMD_TestFile1.txt"
-    echo "TestFile1" > mv_CMD_TestFolder/mv_CMD_TestFile1.txt
-    echo "    echo 'TestFile2' > mv_CMD_TestFolder/mv_CMD_TestFile2.txt"
-    echo "TestFile2" > mv_CMD_TestFolder/mv_CMD_TestFile2.txt
-    echo "    echo 'TestFile3' > mv_CMD_TestFolder/mv_CMD_TestFile3.txt"
-    echo "TestFile3" > mv_CMD_TestFolder/mv_CMD_TestFile3.txt
-    echo "    echo 'TestFile4' > mv_CMD_TestFolder/mv_CMD_TestFile4.txt"
-    echo "TestFile4" > mv_CMD_TestFolder/mv_CMD_TestFile4.txt
-    echo "    echo 'TestFile5' > mv_CMD_TestFolder/mv_CMD_TestFile5.txt"
-    echo "TestFile5" > mv_CMD_TestFolder/mv_CMD_TestFile5.txt
-    echo "    echo 'TestFile6' > mv_CMD_TestFolder/mv_CMD_TestFile6.txt"
-    echo "TestFile6" > mv_CMD_TestFolder/mv_CMD_TestFile6.txt
-    echo "    echo 'TestFile8' > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt"
-    echo "TestFile8" > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt
-    echo "    echo 'TestFile9' > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile4.txt"
-    echo "TestFile9" > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile4.txt
-    echo "    echo 'TestFile10' > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile5.txt"
-    echo "TestFile10" > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile5.txt
-    echo "    echo 'TestFile11' > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile6.txt"
-    echo "TestFile11" > mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile6.txt
-    ls -lR
+    echo "## mv Start ##"
     echo
-    echo " 예시1) mv mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3"
-    mv mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3
-    ls -lR
+    # Basic Information Start
     echo
-    echo " 예시2) mv mv_CMD_TestFolder/mv_CMD_TestFile1.txt mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile2.txt"
-    echo " [목적지 디렉토리]에 파일명까지 지정할 경우 이름이 변경되어 이동한다."
-    mv mv_CMD_TestFolder/mv_CMD_TestFile1.txt mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile2.txt
+    echo " Command  : mv"
+    echo " HowToUse : mv [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : tarFile_Path "
+    echo "            [Argument2] : moveFolder_Path "
+    echo "            [Option : -f] : Move files without asking even if they exist "
+    echo "            [Option : -i] : Move after question when executing command "
+    echo "            [Option : -n] : If the file exists, it does not move "
+    echo "            [Option : -v] : Output detailed information "
     echo
-    echo " 옵션 -f : [목적지 디렉토리]에 파일이 존재하여 파일을 덮어쓸 상황에 확인을 하지 않고 덮어쓴다./ 디폴트 옵션으로 생각된다.."
-    echo " 예시3) mv -f mv_CMD_TestFolder/mv_CMD_TestFile5.txt mv_CMD_TestFolder/mv_CMD_TestFolder2"
-    mv -f mv_CMD_TestFolder/mv_CMD_TestFile5.txt mv_CMD_TestFolder/mv_CMD_TestFolder2
+    # Basic Information End
+    # Preparation Start
     echo
-    echo " 옵션 -i : [목적지 디렉토리]에 동일한 파일명이 있을 경우 이동하여 덮어쓰기를 진행할 것인지 질문을 한다."
-    echo " 예시4) mv -i mv_CMD_TestFolder/mv_CMD_TestFile2.txt mv_CMD_TestFolder/mv_CMD_TestFolder2"
-    mv -i mv_CMD_TestFolder/mv_CMD_TestFile2.txt mv_CMD_TestFolder/mv_CMD_TestFolder2
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3/ "
+    mkdir -p `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3/
+    # Preparation : Create File
+    echo " echo 'TestFile1' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile1.txt "
+    echo 'TestFile1' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile1.txt
+    echo " echo 'TestFile2' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile2.txt "
+    echo 'TestFile2' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile2.txt
+    echo " echo 'TestFile3' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile3.txt "
+    echo 'TestFile3' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile3.txt
+    echo " echo 'TestFile4' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt "
+    echo 'TestFile4' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt
+    echo " echo 'TestFile5' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile5.txt "
+    echo 'TestFile5' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile5.txt
+    echo " echo 'TestFile6' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile6.txt "
+    echo 'TestFile6' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile6.txt
+    echo " echo 'TestFile8' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt "
+    echo 'TestFile8' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt
+    echo " echo 'TestFile9' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile4.txt "
+    echo 'TestFile9' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile4.txt
+    echo " echo 'TestFile10' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile5.txt "
+    echo 'TestFile10' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile5.txt
+    echo " echo 'TestFile11' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile6.txt "
+    echo 'TestFile11' > `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile6.txt
     echo
-    echo " 옵션 -n : [목적지 디렉토리]에 동일한 파일명이 있을 경우 이동하지 않아 덮어쓰기를 진행하지 않는다."
-    echo " 예시5) mv -n mv_CMD_TestFolder/mv_CMD_TestFile4.txt mv_CMD_TestFolder/mv_CMD_TestFolder2"
-    mv -n mv_CMD_TestFolder/mv_CMD_TestFile4.txt mv_CMD_TestFolder/mv_CMD_TestFolder2
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
     echo
-    echo " 옵션 -v : mv 명령어 실행과정을 출력해준다."
-    echo " 예시6) mv -v mv_CMD_TestFolder/mv_CMD_TestFile4.txt mv_CMD_TestFolder/mv_CMD_TestFolder2"
-    mv -v mv_CMD_TestFolder/mv_CMD_TestFile4.txt mv_CMD_TestFolder/mv_CMD_TestFolder2
+    echo " mv `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3/ "
+    mv `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFolder3/
+    echo " ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt "
+    ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile3.txt
+    echo " mv `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile1.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile2.txt "
+    mv `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile1.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/mv_CMD_TestFile2.txt
+    echo " ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile1.txt "
+    ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile1.txt
     echo
-    echo "## 파일(디렉토리) 이동 처리 종료 ##"
-    echo 
+    # Example 02 (Option -f : Move files without asking even if they exist )
+    echo
+    echo " Option -f : Move files without asking even if they exist "
+    echo " mv -f `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile5.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/ "
+    mv -f `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile5.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/
+    echo " ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile5.txt "
+    ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile5.txt
+    echo
+    # Example 03 (Option -i : Move after question when executing command )
+    echo
+    echo " Option -i : Move after question when executing command "
+    echo " mv -i `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile2.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/ "
+    mv -i `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile2.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/
+    echo " ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile2.txt "
+    ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile2.txt
+    echo
+    # Example 04 (Option -n : If the file exists, it does not move )
+    echo
+    echo " Option -n : If the file exists, it does not move "
+    echo " mv -n `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/ "
+    mv -n `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/
+    echo " ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt "
+    ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt
+    echo
+    # Example 05 (Option -v : Output detailed information )
+    echo
+    echo " Option -v : Output detailed information "
+    echo " mv -v `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/ "
+    mv -v `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt `pwd`/mv_CMD_TestFolder/mv_CMD_TestFolder2/
+    echo " ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile3.txt "
+    ls -l `pwd`/mv_CMD_TestFolder/mv_CMD_TestFile4.txt
+    echo
+    # Example End
+    echo
+    echo "## mv End ##"
+    echo
 }
 
-
-MoveFileAndFolder
+mv_MoveFileAndFolder
