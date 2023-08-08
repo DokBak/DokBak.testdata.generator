a149d3e495ab5208a8ce0d4e56b68d20aa471a71
DokBak <photoismylife0216@gmail.com>
2023-08-09 01:03:18 +0900

    FIX : Mkdir_CreateDirectory.sh
    Not Option
    Option -p
    Option -m
    Option -v

--- Folders_Files/Mkdir_CreateDirectory.sh
+++ Folders_Files/Mkdir_CreateDirectory.sh
@@ -1,31 +1,66 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 디렉토리 생성 처리
-#-------------------------
-function CreateDirectory(){
-    echo 
-    echo "## 디렉토리 생성 시작 ##"
-    echo " 명령어  : mkdir"
-    echo " 사용방법 : mkdir [생성할 디렉토리명]"
-    echo " 기본설명 : 현재 작업중인 디렉토리에 [생성할 디렉토리명]에 해당하는 새 폴더를 생성"
-    echo " 예시1) mkdir testdir"
-    mkdir testdir
+#------------------------------------------------------------
+# Cmd Test Shell : [Mkdir]_[CreateDirectory]
+#------------------------------------------------------------
+function mkdir_CreateDirectory(){
     echo
-    echo " 옵션 -p : 상위 디렉토리가 없는 경우, 상위 디렉토리까지 전부 생성"
-    echo " 예시2) mkdir -p testdir2/underTestdir"
-    mkdir -p testdir2/underTestdir
+    echo "## mkdir Start ##"
     echo
-    echo " 옵션 -m : 디렉토리의 권한을 설정하는 옵션"
-    echo " 예시3) mkdir -m 700 testdir3"
-    mkdir -m 700 testdir3
+    # Basic Information Start
     echo
-    echo " 옵션 -v : 디렉토리 생성시 메시지 출력 (p옵션과 조합하면 상위 디렉토리부터 생성한 메시지를 출력한다.)"
-    echo " 예시4) mkdir -v testdir4"
-    mkdir -v testdir4
+    echo " Command  : mkdir"
+    echo " HowToUse : mkdir [Option] [Argument1] "
+    echo "            [Argument1] : createFolder_Path "
+    echo "            [Option : -p] : Create all intermediate directories, even if the directory does not exist "
+    echo "            [Option : -m] : AccessAuthority Set "
+    echo "            [Option : -v] : Output detailed information "
     echo
-    echo "## 디렉토리 생성 종료 ##"
-    echo 
+    # Basic Information End
+    # Preparation Start
+    echo
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " mkdir `pwd`/mkdir_CMD_TestFolder/ "
+    mkdir `pwd`/mkdir_CMD_TestFolder/
+    echo
+    # Example 02 (Option -p : Create all intermediate directories, even if the directory does not exist )
+    echo
+    echo " Option -p : Create all intermediate directories, even if the directory does not exist "
+    echo " ls -Rl `pwd`/mkdir_CMD_TestFolder/ "
+    ls -Rl `pwd`/mkdir_CMD_TestFolder/
+    echo " mkdir -p `pwd`/mkdir_CMD_TestFolder/mkdir_CMD_TestFolder1/mkdir_CMD_TestFolder2/ "
+    mkdir -p `pwd`/mkdir_CMD_TestFolder/mkdir_CMD_TestFolder1/mkdir_CMD_TestFolder2/
+    echo " ls -Rl `pwd`/mkdir_CMD_TestFolder/ "
+    ls -Rl `pwd`/mkdir_CMD_TestFolder/
+    echo
+    # Example 03 (Option -m : AccessAuthority Set )
+    echo
+    echo " Option -m : AccessAuthority Set "
+    echo " ls -Rl `pwd`/mkdir_CMD_TestFolder/ "
+    ls -Rl `pwd`/mkdir_CMD_TestFolder/
+    echo " mkdir -m 700 `pwd`/mkdir_CMD_TestFolder/mkdir_CMD_TestFolder3/ "
+    mkdir -m 700 `pwd`/mkdir_CMD_TestFolder/mkdir_CMD_TestFolder3/
+    echo " ls -Rl `pwd`/mkdir_CMD_TestFolder/ "
+    ls -Rl `pwd`/mkdir_CMD_TestFolder/
+    echo
+    # Example 04 (Option -v : Output detailed information )
+    echo
+    echo " Option -v : Output detailed information "
+    echo " ls -Rl `pwd`/mkdir_CMD_TestFolder/ "
+    ls -Rl `pwd`/mkdir_CMD_TestFolder/
+    echo " mkdir -v `pwd`/mkdir_CMD_TestFolder/mkdir_CMD_TestFolder4/ "
+    mkdir -v `pwd`/mkdir_CMD_TestFolder/mkdir_CMD_TestFolder4/
+    echo " ls -Rl `pwd`/mkdir_CMD_TestFolder/ "
+    ls -Rl `pwd`/mkdir_CMD_TestFolder/
+    echo
+    # Example End
+    echo
+    echo "## mkdir End ##"
+    echo
 }
 
-CreateDirectory
+mkdir_CreateDirectory
