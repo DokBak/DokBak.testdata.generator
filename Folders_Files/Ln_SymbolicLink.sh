d13a4e1ffef208ea53dde7a7cb703ecba07209cb
DokBak <photoismylife0216@gmail.com>
2023-08-11 00:11:49 +0900

    FIX : Ln_SymbolicLink.sh
    Not Option
    Option -s
    Option -f
    Option -v

--- Folders_Files/Ln_SymbolicLink.sh
+++ Folders_Files/Ln_SymbolicLink.sh
@@ -1,41 +1,79 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 심볼릭 링크
-#-------------------------
-function SymbolicLink(){
-    echo 
-    echo "## 심볼릭 링크 시작 ##"
+#------------------------------------------------------------
+# Cmd Test Shell : [Ln]_[SymbolicLink]
+#------------------------------------------------------------
+function ln_SymbolicLink(){
     echo
-    echo " 명령어  : ln"
-    echo " 사용방법 : ln [옵션] [인자1] [인자2]"
-    echo " 기본설명 : 원본 파일[인자1]에 대한 하드 링크 파일[인자2]를 생성"
+    echo "## ln Start ##"
     echo
-    echo " 사전실행) ln 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir ln_CMD_TestFolder/"
-    mkdir -p ln_CMD_TestFolder
-    echo 'test apple' > ln_CMD_TestFolder/ln_CMD_TestFile1.txt
+    # Basic Information Start
     echo
-    echo " 예시1) ln ln_CMD_TestFolder/ln_CMD_TestFile1.txt ln_CMD_TestFolder/ln_CMD_TestFile1_ln.txt"
-    echo " 부가설명) 파일이름은 다르나 커널내에서는 같은 파일로 취급한다. 내부적으로는 같은 파일이기에 한 쪽의 내용이 바뀌면 같이 변경된다."
-    ln ln_CMD_TestFolder/ln_CMD_TestFile1.txt ln_CMD_TestFolder/ln_CMD_TestFile1_ln.txt
-    ls -il ln_CMD_TestFolder
+    echo " Command  : ln"
+    echo " HowToUse : ln [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : tarFile_Path "
+    echo "            [Argument2] : Link_File_Path "
+    echo "            [Option : -s] : Similar to Windows shortcuts "
+    echo "            [Option : -f] : Force an action when overwriting a link that already exists "
+    echo "            [Option : -v] : Output detailed information "
     echo
-    echo " 옵션 -s : 윈도우즈의 바로가기 파일처럼 원본 파일을 가리키는 새로운 파일을 생성한다."
-    echo " 예시2) ln -s ln_CMD_TestFolder/ln_CMD_TestFile1.txt ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt"
-    ln -s ln_CMD_TestFolder/ln_CMD_TestFile1.txt ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt
-    ls -il ln_CMD_TestFolder
+    # Basic Information End
+    # Preparation Start
     echo
-    cat ln_CMD_TestFolder/ln_CMD_TestFile1.txt
-    cat ln_CMD_TestFolder/ln_CMD_TestFile1_ln.txt
-    cat ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt
-    echo 'test apple Test' > ln_CMD_TestFolder/ln_CMD_TestFile1.txt
-    cat ln_CMD_TestFolder/ln_CMD_TestFile1.txt
-    cat ln_CMD_TestFolder/ln_CMD_TestFile1_ln.txt
-    cat ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/ln_CMD_TestFolder/ "
+    mkdir -p `pwd`/ln_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'test apple' > `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt "
+    echo 'test apple' > `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt
     echo
-    echo "## 심볼릭 링크 종료 ##"
-    echo 
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option)
+    echo
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo " ln `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln.txt "
+    ln `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln.txt
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo
+    # Example 02 (Option -s : Similar to Windows shortcuts )
+    echo
+    echo " Option -s : Similar to Windows shortcuts "
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo " ln -s `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt "
+    ln -s `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo
+    # Example 03 (Option -f : Force an action when overwriting a link that already exists )
+    echo
+    echo " Option -f : Force an action when overwriting a link that already exists "
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo " ln -s `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt "
+    ln -s `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt
+    echo " ln -f `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt "
+    ln -f `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln2.txt
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo
+    # Example 04 (Option -v : Output detailed information )
+    echo
+    echo " Option -v : Output detailed information "
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo " ln -v `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln3.txt "
+    ln -v `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1.txt `pwd`/ln_CMD_TestFolder/ln_CMD_TestFile1_ln3.txt
+    echo " ls -il `pwd`/ln_CMD_TestFolder/ "
+    ls -il `pwd`/ln_CMD_TestFolder/
+    echo
+    # Example End
+    echo
+    echo "## ln End ##"
+    echo
 }
 
-SymbolicLink
+ln_SymbolicLink
