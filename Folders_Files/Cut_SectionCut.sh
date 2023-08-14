6443cf3e421f2b64d8f4321e1bbf735a44aa7bfe
DokBak <photoismylife0216@gmail.com>
2023-08-15 00:59:13 +0900

    FIX : Cut_SectionCut.sh
    Not Option
    Option -b
    Option -c
    Option -d
    Option -f

--- Folders_Files/Cut_SectionCut.sh
+++ Folders_Files/Cut_SectionCut.sh
@@ -1,46 +1,69 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 세션 구분 출력
-#-------------------------
-function SectionCut(){
-    echo 
-    echo "## 세션 구분 출력 시작 ##"
-    echo " 명령어  : cut"
-    echo " 사용방법 : cut [출력구분방법] [파일명]"
-    echo " 기본설명 : [파일명]의 내용을 [출력구분방법]에 따라 출력한다. 출력구분방법을 무조건 추가해야한다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Cut]_[SectionCut]
+#------------------------------------------------------------
+function cut_SectionCut(){
     echo
-    echo " 사전실행) cut 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir Cut_CMD_TestFolder/"
-    mkdir -p Cut_CMD_TestFolder
-    echo "    echo 'TestFile1 Testing1' > Cut_CMD_TestFolder/Cut_CMD_TestFile1.txt"
-    echo '2바이트문자1,2바이트문자2,2바이트문자3' > Cut_CMD_TestFolder/Cut_CMD_TestFile1.txt
-    echo '2바이트문자12바이트문자2,2바이트문자3' >> Cut_CMD_TestFolder/Cut_CMD_TestFile1.txt
-    echo '3TestFile1,3Testing2,3Testing3' >> Cut_CMD_TestFolder/Cut_CMD_TestFile1.txt
-    echo '4TestFile14Testing2,4Testing3' >> Cut_CMD_TestFolder/Cut_CMD_TestFile1.txt
-    echo 
-    echo " 옵션 -b n : byte 단위로 구분지어서 출력한다. n번째 바이트 값을 출력한다. 2바이트문자일경우 정확한 값이 출력되지 않는다."
-    echo " 예시1) cut -b 2 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt"
-    cut -b 2 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo "## cut Start ##"
     echo
-    echo " 옵션 -b n-m : byte 단위로 구분지어서 출력한다. n번째 바이트부터 m번째 바이트 값을 출력한다. 2바이트문자일경우 정확한 값이 출력되지 않는다."
-    echo " 예시2) cut -b 1-9 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt"
-    cut -b 1-9 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    # Basic Information Start
     echo
-    echo " 옵션 -c n : char 단위로 구분지어서 출력한다. n번째 char 값을 출력한다."
-    echo " 예시3) cut -c 2 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt"
-    cut -c 2 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo " Command  : cut"
+    echo " HowToUse : cut [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : data Separater "
+    echo "            [Argument2] : tarFile_Path "
+    echo "            [Option : -b] : byte unit Print "
+    echo "            [Option : -c] : character unit Print "
+    echo "            [Option : -d] : Separater unit Print "
+    echo "            [Option : -f] : column Print "
     echo
-    echo " 옵션 -c n-m : char 단위로 구분지어서 출력한다. n번째 char부터 m번째 char 값을 출력한다."
-    echo " 예시4) cut -c 1-9 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt"
-    cut -c 1-9 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    # Basic Information End
+    # Preparation Start
     echo
-    echo " 옵션 -d , -f n : -d뒤의 [,]은 데이터를 나누기위한 구분자, -f뒤의 숫자는 ,로 구분지은 데이터들중에서의 n번째 데이터(n)"
-    echo " 예시5) cut -d , -f 3 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt"
-    cut -d , -f 3 Cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/cut_CMD_TestFolder/ "
+    mkdir -p `pwd`/cut_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo '2byteString1,2byteString2,2byteString3' > `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    echo '2byteString1,2byteString2,2byteString3' > `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo " echo '2byteString12byteString2,2byteString3' >> `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    echo '2byteString12byteString2,2byteString3' >> `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo " echo '3TestFile1,3Testing2,3Testing3' >> `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    echo '3TestFile1,3Testing2,3Testing3' >> `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo " echo '4TestFile14Testing2,4Testing3' >> `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    echo '4TestFile14Testing2,4Testing3' >> `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
     echo
-    echo "## 세션 구분 출력 종료 ##"
-    echo 
+    # Preparation End
+    # Example Start
+    # Example 01 (Option -b : byte unit Print )
+    echo
+    echo " Option -b : byte unit Print "
+    echo " cut -b 5 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    cut -b 5 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo
+    echo " cut -b 2-9 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    cut -b 2-9 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo
+    # Example 02 (Option -c : character unit Print )
+    echo
+    echo " Option -c : character unit Print "
+    echo " cut -c 5 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    cut -c 5 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo
+    echo " cut -c 2-9 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    cut -c 2-9 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo
+    # Example 03 (Option -d : Separater unit Print, Option -f : column Print )
+    echo
+    echo " Option -d : Separater unit Print, Option -f : column Print "
+    echo " cut -d , -f 3 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt "
+    cut -d , -f 3 `pwd`/cut_CMD_TestFolder/cut_CMD_TestFile1.txt
+    echo
+    # Example End
+    echo
+    echo "## cut End ##"
+    echo
 }
 
-SectionCut
+cut_SectionCut
