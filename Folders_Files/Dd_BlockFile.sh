e40aa28c79794cd02993a2644b0969e692f01410
DokBak <photoismylife0216@gmail.com>
2023-08-15 00:58:18 +0900

    FIX : Dd_BlockFile.sh
    Not Option

--- Folders_Files/Dd_BlockFile.sh
+++ Folders_Files/Dd_BlockFile.sh
@@ -1,31 +1,90 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 블록단위 파일 복사 및 변환
-#-------------------------
-function BlockFile(){
-    echo 
-    echo "## 블록 단위 파일 복사 및 변환 시작 ##"
-    echo " 명령어  : dd "
-    echo " 사용방법 : dd []"
-    echo " 기본설명 : 블록 단위로 파일을 복사 하거나 변환하는 명령어, 테스트 파일을 작성시 원하는 용량의 파일을 작성시에도 유용하다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Dd]_[BlockFile]
+#------------------------------------------------------------
+function dd_BlockFile(){
     echo
-    echo " 사전실행) dd 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir -p dd_CMD_TestFolder/dd_CMD_TestFolder2/"
-    mkdir -p dd_CMD_TestFolder/dd_CMD_TestFolder2/
-    echo "    echo 'TestFile1' > dd_CMD_TestFolder/dd_CMD_TestFile1.txt"
-    echo 'TestFile1' > dd_CMD_TestFolder/dd_CMD_TestFile1.txt
-    echo "    echo 'TestFile2' > dd_CMD_TestFolder/dd_CMD_TestFile2.txt"
-    echo 'TestFile2' > dd_CMD_TestFolder/dd_CMD_TestFile2.txt
-    CPWD=$(pwd)
-    echo CPWD : ${CPWD}
+    echo "## dd Start ##"
     echo
-    echo " 예시1) dd if='${CPWD}/dd_CMD_TestFolder/dd_CMD_TestFile1.txt' of='${CPWD}/dd_CMD_TestFolder/dd_CMD_TestFile10.txt' bs=1024 count=10"
-    diff "${CPWD}/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" "${CPWD}/dd_CMD_TestFolder/dd_CMD_TestFile10.txt"
-    dd if="${CPWD}/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="${CPWD}/dd_CMD_TestFolder/dd_CMD_TestFile10.txt" bs=1024 count=10
+    # Basic Information Start
     echo
-    echo "## 블록단위 파일 복사 및 변환 종료 ##"
-    echo 
+    echo " Command  : dd"
+    echo " HowToUse : dd [Argument1] [Argument2] "
+    echo "            [Argument1] : if=inputFile_Path "
+    echo "            [Argument2] : of=outputFile_Path "
+    echo
+    # Basic Information End
+    # Preparation Start
+    echo
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/dd_CMD_TestFolder/ "
+    mkdir -p `pwd`/dd_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'TestFile01TestFile02TestFile03TestFile04TestFile05TestFile06TestFile07TestFile08TestFile09TestFile10TestFile11TestFile12TestFile13TestFile14TestFile15TestFile16TestFile17TestFile18TestFile19TestFile20TestFile21TestFile22TestFile23TestFile24TestFile25TestFile26TestFile27TestFile28TestFile29TestFile30' > `pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt "
+    echo 'TestFile01TestFile02TestFile03TestFile04TestFile05TestFile06TestFile07TestFile08TestFile09TestFile10TestFile11TestFile12TestFile13TestFile14TestFile15TestFile16TestFile17TestFile18TestFile19TestFile20TestFile21TestFile22TestFile23TestFile24TestFile25TestFile26TestFile27TestFile28TestFile29TestFile30' > `pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : if [inputFile_path], of[outputFile_path])
+    echo
+    echo " if : inputFile_path, of : outputFile_path "
+    echo " (if)inputFileSize = 301 -> copy -> (of)outputFileSize = 301 "
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo ' dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile2.txt" '
+    dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile2.txt"
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo
+    # Example 02 (Not Option : bs [blockSize])
+    echo
+    echo " bs : blockSize "
+    echo " (if)inputFileSize = 301 -> (bs)Copy by 64 bytes -> (of)outputFileSize = 64 * 4 + 45 = 301 "
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo ' dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile3.txt" bs=64 '
+    dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile3.txt" bs=64
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo
+    # Example 03 (Not Option : count [blockCount])
+    echo
+    echo " count : blockCount "
+    echo " (if)inputFileSize = 301 -> (bs)Copy by 64 bytes -> (count) 2times -> (of)outputFileSize = 64 * 2 = 128 "
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo ' dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile4.txt" bs=64 count=2 '
+    dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile4.txt" bs=64 count=2
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo
+    # Example 04 (Not Option : skip [skip blockCount from inputFile])
+    echo
+    echo " skip : skip blockCount from inputFile "
+    echo " (if)inputFileSize = 301 -> (bs)Copy by 64 bytes -> (skip) 2times skip -> (of)outputFileSize = 301 - (64 * 2) = 173 "
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo ' dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile5.txt" bs=64 skip=2 '
+    dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile5.txt" bs=64 skip=2
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo
+    # Example 05 (Not Option : seek [skip blockCount from outputFile])
+    echo
+    echo " seek : skip blockCount from outputFile "
+    echo " (if)inputFileSize = 301 -> (bs)Copy by 64 bytes -> (seek) 2times seek -> (of)outputFileSize = (64 * 4 + 45) + (64 * 2) = 301 + 128 = 429 "
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo ' dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile6.txt" bs=64 seek=2 '
+    dd if="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile1.txt" of="`pwd`/dd_CMD_TestFolder/dd_CMD_TestFile6.txt" bs=64 seek=2
+    echo " ls -l `pwd`/dd_CMD_TestFolder/ "
+    ls -l `pwd`/dd_CMD_TestFolder/
+    echo
+    # Example End
+    echo
+    echo "## dd End ##"
+    echo
 }
 
-BlockFile
+dd_BlockFile
