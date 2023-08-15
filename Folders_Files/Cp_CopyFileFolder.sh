47da3991585c727b88390394afddc0d66360eeb2
DokBak <photoismylife0216@gmail.com>
2023-08-16 01:26:55 +0900

    FIX : Cp_CopyFileFolder.sh
    Not Option
    Option -i
    Option -r
    Option -a
    Option -p
    Option -v

--- Folders_Files/Cp_CopyFileFolder.sh
+++ Folders_Files/Cp_CopyFileFolder.sh
@@ -1,54 +1,86 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 파일 디렉토리 복사 처리
-#-------------------------
-function CopyFileFolder(){
-    echo 
-    echo "## 파일 디렉토리 복사 처리 시작 ##"
-    echo " 명령어  : cp "
-    echo " 사용방법 : cp [파일명] [디렉토리명]"
-    echo " 기본설명 : 첫번째 파라미터 파일을 두번째 파라미터 디렉토리에 복사한다."
+#------------------------------------------------------------
+# Cmd Test Shell : [Cp]_[CopyFileFolder]
+#------------------------------------------------------------
+function cp_CopyFileFolder(){
     echo
-    echo " 사전실행) Cp 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir Cp_CMD_TestFolder1"
-    echo "    mkdir Cp_CMD_TestFolder2"
-    echo "    mkdir Cp_CMD_TestFolder3"
-    mkdir Cp_CMD_TestFolder1
-    mkdir Cp_CMD_TestFolder2
-    mkdir Cp_CMD_TestFolder3
-    echo "    echo 'TestFile1' > Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt"
-    echo "TestFile1" > Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt
+    echo "## cp Start ##"
     echo
-    echo " 예시1) cp Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder2"
-    cp Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder2
+    # Basic Information Start
     echo
-    echo " 예시2) cp Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder2/Cp_CMD_TestFile2.txt"
-    echo "부가 설명 : [디렉토리명] 자리에 파일명까지 지정할 경우 해당 파일명으로 이름이 바뀌어 복사된다."
-    cp Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder2/Cp_CMD_TestFile2.txt
+    echo " Command  : cp"
+    echo " HowToUse : cp [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : tarFile_Path1 "
+    echo "            [Argument2] : copyFile_Path2 "
+    echo "            [Option : -i] : If the file exists, check if it is overwritten "
+    echo "            [Option : -r] : Recursively copy files in the directory "
+    echo "            [Option : -a] : Copy the properties and link information of the original file "
+    echo "            [Option : -p] : Copy information such as owner group, permission, etc. of the original file "
+    echo "            [Option : -v] : Show progress "
     echo
-    echo " 옵션 -i : 복사될 파일이 이름이 이미 존재할 경우, 덮어쓰기 여부를 확인한다.(옵션이 없다면 자동복사, 옵션을 사용해도 이미 존재하지 않는 경우는 그냥 복사)"
-    echo " 예시3) cp -i Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder2/Cp_CMD_TestFile2.txt"
-    cp -i Cp_CMD_TestFolder1/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder2/Cp_CMD_TestFile2.txt
+    # Basic Information End
+    # Preparation Start
     echo
-    echo " 옵션 -r : 하위 디렉토리까지 생성한다."
-    echo " 예시4) cp -r Cp_CMD_TestFolder2 Cp_CMD_TestFolder3"
-    cp -r Cp_CMD_TestFolder2 Cp_CMD_TestFolder3
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/cp_CMD_TestFolder1/ "
+    mkdir -p `pwd`/cp_CMD_TestFolder1/
+    echo " mkdir -p `pwd`/cp_CMD_TestFolder2/ "
+    mkdir -p `pwd`/cp_CMD_TestFolder2/
+    echo " mkdir -p `pwd`/cp_CMD_TestFolder3/ "
+    mkdir -p `pwd`/cp_CMD_TestFolder3/
+    # Preparation : Create File
+    echo " echo 'TestFile1' > `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt "
+    echo 'TestFile1' > `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt
     echo
-    echo " 옵션 -a : 원본 파일의 속성, 링크정보까지 복사한다."
-    echo " 예시5) cp -a Cp_CMD_TestFolder2/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder3"
-    cp -a Cp_CMD_TestFolder2/Cp_CMD_TestFile1.txt Cp_CMD_TestFolder3
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option : file copy )
     echo
-    echo " 옵션 -p : 원본 파일의 소유자 그룹, 권한 등의 정보까지 복사한다."
-    echo " 예시6) cp -p Cp_CMD_TestFolder2/Cp_CMD_TestFile2.txt Cp_CMD_TestFolder3"
-    cp -p Cp_CMD_TestFolder2/Cp_CMD_TestFile2.txt Cp_CMD_TestFolder3
+    echo " Not Option : file copy "
+    echo " cp `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder2/ "
+    cp `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder2/
     echo
-    echo " 옵션 -v :복사의 진행상태를 표시한다."
-    echo " 예시7) cp -rv Cp_CMD_TestFolder3 Cp_CMD_TestFolder4"
-    cp -rv Cp_CMD_TestFolder3 Cp_CMD_TestFolder4
+    # Example 02 (Not Option : file copy and rename )
     echo
-    echo "## 파일 디렉토리 복사 처리 종료 ##"
-    echo 
+    echo " Not Option : file copy and rename "
+    echo " cp `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile2.txt "
+    cp `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile2.txt
+    echo
+    # Example 03 (Option -i : If the file exists, check if it is overwritten )
+    echo
+    echo " Option -i : If the file exists, check if it is overwritten "
+    echo " cp -i `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile2.txt "
+    cp -i `pwd`/cp_CMD_TestFolder1/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile2.txt
+    echo
+    # Example 04 (Option -r : Recursively copy files in the directory )
+    echo
+    echo " Option -r : Recursively copy files in the directory "
+    echo " cp -r `pwd`/cp_CMD_TestFolder2/ `pwd`/cp_CMD_TestFolder3/ "
+    cp -r `pwd`/cp_CMD_TestFolder2/ `pwd`/cp_CMD_TestFolder3/
+    echo
+    # Example 05 (Option -a : Copy the properties and link information of the original file )
+    echo
+    echo " Option -a : Copy the properties and link information of the original file " 
+    echo " cp -a `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder3/ "
+    cp -a `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile1.txt `pwd`/cp_CMD_TestFolder3/
+    echo
+    # Example 06 (Option -p : Copy information such as owner group, permission, etc. of the original file )
+    echo
+    echo " Option -p : Copy information such as owner group, permission, etc. of the original file "
+    echo " cp -p `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile2.txt `pwd`/cp_CMD_TestFolder3/ "
+    cp -p `pwd`/cp_CMD_TestFolder2/cp_CMD_TestFile2.txt `pwd`/cp_CMD_TestFolder3/
+    echo
+    # Example 07 (Option -v : Show progress )
+    echo
+    echo " Option -v : Show progress "
+    echo " cp -rv `pwd`/cp_CMD_TestFolder3/ `pwd`/cp_CMD_TestFolder4/ "
+    cp -rv `pwd`/cp_CMD_TestFolder3/ `pwd`/cp_CMD_TestFolder4/
+    echo
+    # Example End
+    echo
+    echo "## cp End ##"
+    echo
 }
 
-CopyFileFolder
+cp_CopyFileFolder
