bb877fe9a15b5f7dc6b57bd70ca13dcbc7ff71c2
DokBak <photoismylife0216@gmail.com>
2023-08-19 11:35:52 +0900

    FIX : Chgrp_FileGroupEdit.sh
    Not Option
    Option -R

--- Folders_Files/Chgrp_FileGroupEdit.sh
+++ Folders_Files/Chgrp_FileGroupEdit.sh
@@ -1,33 +1,55 @@
 #!/bin/sh
 
-#-------------------------
-# 테스트 쉘 : 파일 그룹 변경
-#-------------------------
-function FileGroupEdit(){
-    echo 
-    echo "## 파일 그룹 변경 시작 ##"
+#------------------------------------------------------------
+# Cmd Test Shell : [Chgrp]_[FileGroupEdit]
+#------------------------------------------------------------
+function chgrp_FileGroupEdit(){
     echo
-    echo " 명령어  : chgrp"
-    echo " 사용방법 : chgrp [옵션] [소유자명] [파일명]"
-    echo " 기본설명 : 파일의 소유자명을 [소유자명]으로 변경"
+    echo "## chgrp Start ##"
     echo
-    echo " 사전실행) chgrp 명령어 실행에 필요한 샘플 폴더 및 파일 작성 방법"
-    echo "    mkdir chgrp_CMD_TestFolder/"
-    mkdir -p chgrp_CMD_TestFolder
-    echo 'test apple' > chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt
-    ls -l chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt
+    # Basic Information Start
     echo
-    echo " 예시1) sudo chgrp dokg chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt"
-    sudo chgrp dokg chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt
-    ls -l chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt
+    echo " Command  : chgrp"
+    echo " HowToUse : chgrp [Option] [Argument1] [Argument2] "
+    echo "            [Argument1] : owner group "
+    echo "            [Argument2] : tarFile_Path "
+    echo "            [Option : -R] : Apply to subfolders and files as well "
     echo
-    echo " 옵션 -R : 디렉토리를 지정할 경우 하위 내용까지 모두 동일한 권한을 부여한다."
-    echo " 예시2) sudo chgrp -R staff chgrp_CMD_TestFolder"
-    sudo chgrp -R staff chgrp_CMD_TestFolder
-    ls -lR
+    # Basic Information End
+    # Preparation Start
     echo
-    echo "## 파일 그룹 변경 종료 ##"
-    echo 
+    # Preparation : mkdir
+    echo " mkdir -p `pwd`/chgrp_CMD_TestFolder/ "
+    mkdir -p `pwd`/chgrp_CMD_TestFolder/
+    # Preparation : Create File
+    echo " echo 'test apple' > `pwd`/chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt "
+    echo 'test apple' > `pwd`/chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt
+    echo " ls -l `pwd`/chgrp_CMD_TestFolder/ "
+    ls -l `pwd`/chgrp_CMD_TestFolder/
+    echo
+    # Preparation End
+    # Example Start
+    # Example 01 (Not Option )
+    echo
+    echo " sudo Chgrp admin `pwd`/chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt "
+    sudo Chgrp admin `pwd`/chgrp_CMD_TestFolder/chgrp_CMD_TestFile1.txt
+    echo " ls -l `pwd`/chgrp_CMD_TestFolder/ "
+    ls -l `pwd`/chgrp_CMD_TestFolder/
+    echo
+    # Example 02 (Option -R : Apply to subfolders and files as well )
+    echo
+    echo " Option -R : Apply to subfolders and files as well "
+    echo " sudo Chgrp -R wheel `pwd`/chgrp_CMD_TestFolder/ "
+    sudo Chgrp -R wheel `pwd`/chgrp_CMD_TestFolder/
+    echo " ls -l `pwd`/ "
+    ls -l `pwd`/
+    echo " ls -l `pwd`/chgrp_CMD_TestFolder/ "
+    ls -l `pwd`/chgrp_CMD_TestFolder/
+    echo
+    # Example End
+    echo
+    echo "## chgrp End ##"
+    echo
 }
 
-FileGroupEdit
+chgrp_FileGroupEdit
