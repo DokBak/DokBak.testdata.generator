84a72aec4e9fb57257463a99a7eec8d30762abcd
DokBak <photoismylife0216@gmail.com>
2024-02-23 23:31:31 +0900

    UPDATE : ShellScriptCommand.sh
    Main Menu Page Create
    메인메뉴 페이지 작성
    メインメニューページ作成

--- ShellScriptCommand.sh
+++ ShellScriptCommand.sh
@@ -11,73 +11,220 @@
 #  Processing Overview : Reference Shell for Writing a Shell Script               #
 #                                                                                 #
 #  Parameter   :                                                                  #
-#  Parameter1  (optional) : Language                                              #
-#  Parameter2  (optional) : Command                                               #
+#     Parameter1  (optional) : Command                                            #
+#     Parameter2  (optional) : Language                                           #
 #                                                                                 #
 ###################################################################################
 #                                                                                 #
-#  쉘  이름      : 참조 쉘                                                            #
+#  쉘  이름      : 참조 쉘                                                          　#
 #                                                                                 #
-#  작 성 자      : DokBak                                                           #
+#  작 성 자      : DokBak                                                        　　#
 #  작 성 일      : 2024/2/21          신규 작성                                       #
-#  수 정 일      :                                                                  #
+#  수 정 일      :                                                               　　#
 #                                                                                 #
-#  처리 개요      : 쉘 스크립트 작성을 위한 참조 쉘                                         #
+#  처리 개요      : 쉘 스크립트 작성을 위한 참조 쉘                                       　#
 #                                                                                 #
-#  파라미터       :                                                                  #
-#  파라미터1  (옵션) : 언어                                                            #
-#  파라미터2  (옵션) : 명령어                                                          #
+#  파라미터       :                                                             　　　#
+#     파라미터1  (옵션) : 명령어                                                       #
+#     파라미터2  (옵션) : 언어                                                       　#
 #                                                                                 #
 ###################################################################################
 #                                                                                 #
-#  スクリプト名  : 参照シェル                                                           #
+#  スクリプト名    : 参照シェル                                                         #
 #                                                                                 #
-#  作成者     : DokBak                                                              #
-#  作成日 : 2024/2/21          新規作成                                               #
-#  修正日 :                                                                         #
+#  作成者        : DokBak                                                        　　#
+#  作成日        : 2024/2/21          新規作成                                        #
+#  修正日        :                                                               　　#
 #                                                                                 #
-#  処理概要 : シェルスクリプト作成に参考できる参照シェル                                        #
+#  処理概要      : シェルスクリプト作成に参考できる参照シェル                                 　#
 #                                                                                 #
-#  パラメータ   :                                                                    #
-#  パラメータ1  (任意) : 言語                                                          #
-#  パラメータ2  (任意) : 命令語                                                         #
+#  パラメータ     :                                                               　　#
+#     パラメータ1  (任意) : 言語                                                       #
+#     パラメータ2  (任意) : 命令語                                                   　　#
 #                                                                                 #
 ###################################################################################
 
 #--------------------------------------------#
-# languageCheck                           #
+# PrintMenu                                  #
+#  : Main Menu Print                         #
+#  : 메인 메뉴 출력                            　#
+#  : メインメニュー出力                           #
 #--------------------------------------------#
-function languageCheck() {
+function PrintMenu() {
 
     ### Language Parameter / 언어 파라미터 / 言語パラメータ
     languageParam=$1
 
-    ### Parameter exist / 파라미터가 존재 / パラメータが存在
-    if [ ${languageParam} = "en" ]; then
-        functionOutput="### Output language is English. ###"
-    elif [ ${languageParam} = "kr" ]; then
-        functionOutput="### 출력 언어는 한국어입니다. ###"
-    elif [ ${languageParam} = "jp" ]; then
-        functionOutput="### 出力言語は日本語です。 ###"
-    else
-        functionOutput="### Please check the parameters.(Only lowercase letters : en, kr, jp) ###"
-    fi
-
-    ### Regular Expression / 정규표현식 / 正規表現式 
-    ### Parameter exist / 파라미터가 존재 / パラメータが存在
-    if [[ ${languageParam} == [eE][nN] ]]; then
-        functionOutput="### Output language is English. ###"
-    elif [[ ${languageParam} == [kK][rR] ]]; then
-        functionOutput="### 출력 언어는 한국어입니다. ###"
+    ### Function Main Logic / 함수 메인 로직 / 関数メインロジック
+    if [[ ${languageParam} == [kK][rR] ]];then
+        echo
+        printf "  ******************************\n"
+        printf "  *%18s%12s*\n"                     "메뉴"
+        printf "  ******************************\n"
+        printf "  * %-31s*\n"                       "1. 언어 선택"
+        printf "  * %-36s*\n"                       "2. 리눅스 명령어 리스트"
+        printf "  * %-36s*\n"                       "3. 스크립트 작성 도움말"
+        printf "  *                            *\n"
+        printf "  * %-29s*\n"                       "9. 종료"
+        printf "  ******************************\n"
+        echo 
+        read -p " 메뉴 선택 : " SelectMenu
+        echo
     elif [[ ${languageParam} == [jJ][pP] ]]; then
-        functionOutput="### 出力言語は日本語です。 ###"
+        echo
+        printf "  ******************************\n"
+        printf "  *%22s%10s*\n"                     "メニュー"
+        printf "  ******************************\n"
+        printf "  * %-31s*\n"                       "1. 言語選択"
+        printf "  * %-39s*\n"                       "2. リナックスコマンドリスト"
+        printf "  * %-36s*\n"                       "3. スクリプトのヘルプ"
+        printf "  *                            *\n"
+        printf "  * %-29s*\n"                       "9. 終了"
+        printf "  ******************************\n"
+        echo 
+        read -p " メニュー選択 : " SelectMenu
+        echo
     else
-        functionOutput="### Please check the parameters.(case-insensitive : en, kr, jp) ###"
+        echo
+        printf "  ******************************\n"
+        printf "  *%16s%12s*\n"                     "MENU"
+        printf "  ******************************\n"
+        printf "  * %-27s*\n"                       "1. Select Language"
+        printf "  * %-27s*\n"                       "2. List of Linux Commands"
+        printf "  * %-27s*\n"                       "3. Helping For Script"
+        printf "  *                            *\n"
+        printf "  * %-27s*\n"                       "9. End"
+        printf "  ******************************\n"
+        echo 
+        read -p " Select Menu : " SelectMenu
+        echo
+    
     fi
     
-    ### Result print / 결과 출력 / 結果出力
-    echo ${functionOutput}
+    ### Shell Script End / 쉘 스크립트 종료 / シェルスクリプト終了
+    if [[ ${SelectMenu} == "終了" || ${SelectMenu} == "종료" || ${SelectMenu} == [eE][nN][dD] || ${SelectMenu} == [eE][xX][iI][tT] ||  ${SelectMenu} == 9 ]];then
+        if [[ ${languageParam} == [kK][rR]  ]];then
+            echo
+            printf "  ******************************\n"
+            printf "  *                            *\n"
+            printf "  * %36s%2s*\n"                     "사용해주셔서 감사합니다"
+            printf "  * %17s%10s*\n"                    "[DokBak]"
+            printf "  *                            *\n"
+            printf "  ******************************\n"
+            echo
+            break
+        elif [[ ${languageParam} == [jJ][pP]  ]];then
+            echo
+            printf "  ******************************\n"
+            printf "  *                            *\n"
+            printf "  * %19s%1s*\n"                     "ご利用ありがとうございます"
+            printf "  * %17s%10s*\n"                    "[DokBak]"
+            printf "  *                            *\n"
+            printf "  ******************************\n"
+            echo
+            break
+        else
+            echo
+            printf "  ******************************\n"
+            printf "  *                            *\n"
+            printf "  * %22s%5s*\n"                     "Thank you for Using"
+            printf "  * %17s%10s*\n"                    "[DokBak]"
+            printf "  *                            *\n"
+            printf "  ******************************\n"
+            echo
+            break
+        fi
+    fi
+}
 
+#--------------------------------------------#
+# Selectlanguage                             #
+#--------------------------------------------#
+function Selectlanguage() {
+
+    ### Language Parameter / 언어 파라미터 / 言語パラメータ
+    languageParam=$1
+
+    ### Function Main Logic / 함수 메인 로직 / 関数メインロジック
+    while true
+    do
+        if [[ ${languageParam} == [kK][rR] ]];then
+            echo
+            printf "  ******************************\n"
+        printf "  * %-31s*\n"                       "1. 언어 선택"
+            echo "  *          언어 선택           *"
+            printf "  ******************************\n"
+            echo "  * 1. 영어(en)                  *"
+            echo "  * 2. 한국어(kr)                *"
+            echo "  * 3. 일본어(jp)                *"
+            echo "  *                            *"
+            echo "  * 9. 이전메뉴                  *"
+            printf "  ******************************\n"
+            echo 
+            read -p " 출력 언어를 선택해주세요. : " SelectLanguage
+            echo
+        elif [[ ${languageParam} == [jJ][pP] ]]; then
+            echo
+            echo "  ******************************"
+            echo "  *          言語選択           *"
+            echo "  ******************************"
+            echo "  * 1. 英語(en)                 *"
+            echo "  * 2. 韓国語(kr)                *"
+            echo "  * 3. 日本語(jp)                *"
+            echo "  *                            *"
+            echo "  * 9. 前のメニュー                *"
+            echo "  ******************************"
+            echo 
+            read -p " 出力言語を選択ください。 : " SelectLanguage
+            echo
+        else
+            echo
+            echo "  ******************************"
+            echo "  *      Select Language      *"
+            echo "  ******************************"
+            echo "  * 1. English(en)             *"
+            echo "  * 2. Korean(kr)              *"
+            echo "  * 3. Japense(jp)             *"
+            echo "  *                            *"
+            echo "  * 9. Previous Menu           *"
+            echo "  ******************************"
+            echo 
+            read -p " Select Language. : " SelectLanguage
+            echo
+        fi
+
+        ### ** Regular Expression logic / 정규표현식 처리 / 正規表現式ロジック 
+        ### Parameter exist / 파라미터가 존재 / パラメータが存在
+        if [[ ${SelectLanguage} == 1 || ${SelectLanguage} == [eE][nN] ]]; then
+            languageParam="en"
+            OuputLanguage="en"
+            break
+        elif [[ ${SelectLanguage} == 2 || ${SelectLanguage} == [kK][rR] ]]; then
+            languageParam="kr"
+            OuputLanguage="kr"
+            break
+        elif [[ ${SelectLanguage} == 3 || ${SelectLanguage} == [jJ][pP] ]]; then
+            languageParam="jp"
+            OuputLanguage="jp"
+            break
+        else
+            if [[ ${languageParam} == [eE][nN] ]]; then
+                echo "### Please check the language. (case-insensitive : en, kr, jp) ###"
+                echo "### Please check the Number.   (                    1,  2, 3 ) ###"
+            elif [[ ${languageParam} == [kK][rR] ]]; then
+                echo "### 출력 언어를 선택해주세요. (대소문자 구분없음 : en, kr, jp) ###"
+                echo "### 출력 번호를 선택해주세요. (                 1,  2, 3 ) ###"
+            elif [[ ${languageParam} == [jJ][pP] ]]; then
+                echo "### 出力言語を選択してください. (大小文字区別無し : en, kr, jp) ###"
+                echo "### 出力番号を選択してください. (                1,  2, 3 ) ###"
+            fi
+        fi
+
+        if [ ${SelectLanguage} = 9 ]; then
+            break
+        fi
+
+    done
 }
 
 #--------------------------------------------#
@@ -182,89 +329,40 @@ function basicSetting_FileName_Path() {
 paramCount=$#
 ### ProcessID / 프로세스ID / プロセスID
 PID=$$
-
-### Language select / 언어 선택 / 言語選択
-while true
-do
-    if [[ ${paramCount} == 0 ]];then
-        echo "Please enter a language"
-        read -p "Insert(en, kr, jp) : " OuputLanguage
-    else
-        OuputLanguage=$1
-    fi
-    ### English, Korean, Japanese / 영어, 한국어, 일본어 / 英語、韓国語、日本語
-    if [[ ${OuputLanguage} == [eE][nN] || ${OuputLanguage} == [kK][rR] || ${OuputLanguage} == [jJ][pP] ]]; then
-        break
-    fi
-done
-echo
-
-### Function Run / 함수 실행 / 関数実行
-languageCheck ${OuputLanguage} ${Command}
-### Function Run / 함수 실행 / 関数実行
-basicSetting_RunTime ${OuputLanguage} 
-echo
-### Function Run / 함수 실행 / 関数実行
-basicSetting_FileName_Path ${OuputLanguage} ${PID} "0" 
-echo
-
 ### SearchCommand / 검색명령어 / 検索コマンド
-SearchCommand=$2
+SearchCommand=$1
+### Output Language / 출력 언어 / 出力言語
+OuputLanguage=$2
+#--------------------------------------------#
+# Main logic                                 #
+#--------------------------------------------#
+### Parameter Check / 파라미터 체크 / パラメータチェック
+if [[ ${paramCount} == 0 || ${paramCount} == 1 ]];then
+    echo 
+    echo "##############################################################################################"
+    echo
+    echo "  Default language for this shell script is English "
+    echo "  Support language : English(en), Korean(kr), Japense(jp) "
+    echo "  How To Use : sh [Shell Scrip Directory Path]/ShellScriptCommand.sh [Command] [Language] "
+    echo
+    echo "  Sample : sh ./ShellScriptCommand.sh "
+    echo "         : sh ./ShellScriptCommand.sh cp "
+    echo "         : sh ./ShellScriptCommand.sh cat kr "
+    echo
+    echo "##############################################################################################"
+    echo
+fi
 
-### Command List / 명령어 리스트 / コマンドリスト
-declare -a CommandList=("cat1" "cd1" "cut1")
-declare -a CommandDescription=("cat2" "cd2" "cut2")
-
-echo ${CommandList[@]}
-echo ${#CommandList[@]}
-echo
-Index=0
-for CommandIndex in ${CommandList[@]}; do
-    echo CommandList[${Index}]=${CommandList[${Index}]}, CommandDescription[${Index}]=${CommandDescription[${Index}]}
-    Index=$((${Index} + 1))
-done
-
-
-
-### Command Select / 명령어 선택 / コマンド選択
 while true
 do
-    if [[ ${SearchCommand} == "" ]];then
-        if [[ ${OuputLanguage} == [eE][nN] ]]; then
-            echo "Shell Stop : ZZ"
-            read -p "Please enter a Command." SearchCommand
-        elif [[ ${OuputLanguage} == [kK][rR] ]]; then
-            echo "쉘 종료 : ZZ"
-            read -p "명령어를 입력해주세요." SearchCommand
-        elif [[ ${OuputLanguage} == [jJ][pP] ]]; then
-            echo "シェル終了 : ZZ"
-            read -p "コマンドを入力してください。" SearchCommand
-        fi
-    fi
-    ### Command Example / 명령어 예시 / コマンドサンプル
-    if [[ ${SearchCommand} == "dd" ]]; then
-        basicSetting_FileName_Path ${OuputLanguage} ${PID} "0" ${SearchCommand} 
-        echo
-        echo "각 명령어당 작성 함수"
-        echo
-        basicSetting_FileName_Path ${OuputLanguage} ${PID} "1" ${SearchCommand} 
-    elif [[ ${SearchCommand} == [zZ][zZ] ]]; then
-        basicSetting_FileName_Path ${OuputLanguage} ${PID} "1" 
-        break
+    ### Function Run / 함수 실행 / 関数実行
+    PrintMenu ${OuputLanguage}
+
+    if [[ ${SelectMenu} == 1 ]];then
+        ### Function Run / 함수 실행 / 関数実行
+        Selectlanguage ${OuputLanguage}
     else
-        if [[ ${OuputLanguage} == [eE][nN] ]]; then
-            echo "The command isn't in the shell."
-            echo "This command(${SearchCommand}) will be added."
-        elif [[ ${OuputLanguage} == [kK][rR] ]]; then
-            echo "쉘에 존재하지 않는 명령어입니다."
-            echo "해당 명령어(${SearchCommand})는 추가예정입니다."
-        elif [[ ${OuputLanguage} == [jJ][pP] ]]; then
-            echo "シェルに存在しないコマンドです。"
-            echo "該当コマンド（${SearchCommand}）は追加予定です。"
-        fi
-    fi
-    SearchCommand=""
+        continue
+    fi 
+
 done
-
-
-
